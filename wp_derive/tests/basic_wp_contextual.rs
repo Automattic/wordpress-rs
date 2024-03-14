a106fce226d583c3b18a311194637db8ff961c8a
Oguz Kocer <oguz.kocer@automattic.com>
2024-03-14 15:15:44 -0400

    Fix basic_wp_contextual test not calling all context types

--- wp_derive/tests/basic_wp_contextual.rs
+++ wp_derive/tests/basic_wp_contextual.rs
@@ -8,8 +8,8 @@ pub struct SparseFoo {
 
 fn main() {
     let _ = FooWithEditContext { bar: 0 };
-    let _ = FooWithEditContext { bar: 0 };
-    let _ = FooWithEditContext { bar: 0 };
+    let _ = FooWithEmbedContext { bar: 0 };
+    let _ = FooWithViewContext { bar: 0 };
 }
 
 uniffi::setup_scaffolding!("wp_derive");
