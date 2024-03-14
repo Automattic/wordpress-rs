81620f87aa9a64ccebf702e76ccf69db3a882f24
Oguz Kocer <oguz.kocer@automattic.com>
2024-03-14 15:30:42 -0400

    Adds a test for missing 'Sparse' prefix while using WPContextual

--- wp_derive/tests/all_tests.rs
+++ wp_derive/tests/all_tests.rs
@@ -3,4 +3,5 @@ fn tests() {
     let t = trybuild::TestCases::new();
     t.pass("tests/basic_wp_contextual.rs");
     t.pass("tests/basic_wp_contextual_field.rs");
+    t.compile_fail("tests/error_missing_sparse_prefix_from_wp_contextual.rs");
 }
