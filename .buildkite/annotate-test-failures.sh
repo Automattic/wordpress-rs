ae3e7bef634ed044239d47006ce8341212b565c3
Tony Li <tony.li@automattic.com>
2024-03-14 18:58:44 +1300

    Try docker

--- .buildkite/annotate-test-failures.sh
+++ .buildkite/annotate-test-failures.sh
@@ -13,4 +13,24 @@ fi
 
 echo "--- :buildkite: Annotate test failures"
 
-test -f ".build/spm-all-tests.xunit.xml" && annotate_test_failures ".build/spm-all-tests.xunit.xml"
+function annotate {
+    local junit_file; junit_file=$1
+
+    if command -v ruby &> /dev/null
+    then
+        annotate_test_failures "$junit_file"
+        return
+    fi
+
+    echo "Ruby not found, using docker to run the annotation utility"
+    plugin_dir=$(echo "$PATH" | tr ':' '\n' | grep -F a8c-ci-toolkit)
+    docker run --rm -it \
+        -v "$(pwd):/app:ro" \
+        -v "${plugin_dir}:${plugin_dir}:ro" \
+        -w /app \
+        --entrypoint /bin/sh \
+        public.ecr.aws/docker/library/ruby:3.2-alpine \
+        -c "export PATH=$plugin_dir:\$PATH; annotate_test_failures '$junit_file'"
+}
+
+test -f ".build/spm-all-tests.xunit.xml" && annotate ".build/spm-all-tests.xunit.xml"
