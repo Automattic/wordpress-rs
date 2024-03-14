b91ada810fd66f3154eec99c2584f957a52b0a20
Tony Li <tony.li@automattic.com>
2024-03-14 13:43:43 +1300

    Test: create a test failure

--- native/swift/Tests/wordpress-api/WordPressAPITests.swift
+++ native/swift/Tests/wordpress-api/WordPressAPITests.swift
@@ -8,6 +8,6 @@ final class WordPressAPITests: XCTestCase {
     func testExample() {
         let request = WpApiHelper(siteUrl: "https://wordpress.org", authentication: .init(authToken: "token"))
             .postListRequest(params: .init())
-        XCTAssertTrue(request.url.hasPrefix("https://wordpress.org/wp-json/wp/v2/posts"))
+        XCTAssertFalse(request.url.hasPrefix("https://wordpress.org/wp-json/wp/v2/posts"))
     }
 }
