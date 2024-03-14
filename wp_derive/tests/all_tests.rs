81db28198b63607600360c296e05e662fb466e8e
Oguz Kocer <oguz.kocer@automattic.com>
2024-03-14 15:42:49 -0400

    Add a test for missing Sparse prefix when marked with #[WPContextualField]

--- wp_derive/tests/all_tests.rs
+++ wp_derive/tests/all_tests.rs
@@ -4,4 +4,5 @@ fn tests() {
     t.pass("tests/basic_wp_contextual.rs");
     t.pass("tests/basic_wp_contextual_field.rs");
     t.compile_fail("tests/error_missing_sparse_prefix_from_wp_contextual.rs");
+    t.compile_fail("tests/error_missing_sparse_prefix_from_wp_contextual_field.rs");
 }
