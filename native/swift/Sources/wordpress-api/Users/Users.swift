e883c52500d23f7917798b911b333de6e400c2c3
Tony Li <tony.li@automattic.com>
2024-04-11 16:00:19 +1200

    Add APIs to read view/edit/embed context

--- native/swift/Sources/wordpress-api/Users/Users.swift
+++ native/swift/Sources/wordpress-api/Users/Users.swift
@@ -1,16 +1,24 @@
 import Foundation
 import wordpress_api_wrapper
 
+extension SparseUser: Contextual {}
+
 extension WordPressAPI {
-    public var users: Namespace<SparseUser> {
+    public var users: AnyNamespace<SparseUser> {
         .init(api: self)
     }
 }
 
-extension Namespace where T == SparseUser {
+extension ViewNamespace where T == SparseUser {
 
     public func get(id: T.ID) async throws -> T.View {
         let request = self.api.helper.retrieveUserRequest(userId: id, context: .view)
+        let response = try await api.perform(request: request)
+        return try parseRetrieveUserResponseWithViewContext(response: response)!
+    }
+
+    public func getCurrent() async throws -> T.View {
+        let request = self.api.helper.retrieveCurrentUserRequest(context: .view)
         let response = try await api.perform(request: request)
         return try parseRetrieveUserResponseWithViewContext(response: response)!
     }
@@ -20,6 +28,56 @@ extension Namespace where T == SparseUser {
         let response = try await api.perform(request: request)
         return try parseListUsersResponseWithViewContext(response: response)
     }
+
+}
+
+extension EditNamespace where T == SparseUser {
+
+    public func get(id: T.ID) async throws -> T.Edit {
+        let request = self.api.helper.retrieveUserRequest(userId: id, context: .edit)
+        let response = try await api.perform(request: request)
+        return try parseRetrieveUserResponseWithEditContext(response: response)!
+    }
+
+    public func getCurrent() async throws -> T.Edit {
+        let request = self.api.helper.retrieveCurrentUserRequest(context: .edit)
+        let response = try await api.perform(request: request)
+        return try parseRetrieveUserResponseWithEditContext(response: response)!
+    }
+
+    public func list() async throws -> [T.Edit] {
+        let request = self.api.helper.listUsersRequest(context: .edit, params: nil)
+        let response = try await api.perform(request: request)
+        return try parseListUsersResponseWithEditContext(response: response)
+    }
+
+}
+
+extension EmbedNamespace where T == SparseUser {
+
+    public func get(id: T.ID) async throws -> T.Embed {
+        let request = self.api.helper.retrieveUserRequest(userId: id, context: .edit)
+        let response = try await api.perform(request: request)
+        return try parseRetrieveUserResponseWithEmbedContext(response: response)!
+    }
+
+    public func getCurrent() async throws -> T.Embed {
+        let request = self.api.helper.retrieveCurrentUserRequest(context: .edit)
+        let response = try await api.perform(request: request)
+        return try parseRetrieveUserResponseWithEmbedContext(response: response)!
+    }
+
+    public func list() async throws -> [T.Embed] {
+        let request = self.api.helper.listUsersRequest(context: .edit, params: nil)
+        let response = try await api.perform(request: request)
+        return try parseListUsersResponseWithEmbedContext(response: response)
+    }
+
+}
+
+// MARK: - Edit context
+
+extension AnyNamespace where T == SparseUser {
 
     public func delete(id: T.ID, reassignTo userID: T.ID) async throws {
         let request = self.api.helper.deleteUserRequest(userId: id, params: .init(reassign: userID))
@@ -38,16 +96,6 @@ extension Namespace where T == SparseUser {
         let request = self.api.helper.createUserRequest(params: params)
         let response = try await self.api.perform(request: request)
         return try parseRetrieveUserResponseWithEditContext(response: response)!
-    }
-
-}
-
-extension Namespace where T == SparseUser {
-
-    public func getCurrent() async throws -> T.View {
-        let request = self.api.helper.retrieveCurrentUserRequest(context: .view)
-        let response = try await api.perform(request: request)
-        return try parseRetrieveUserResponseWithViewContext(response: response)!
     }
 
     public func deleteCurrent(reassignTo userID: T.ID) async throws {
