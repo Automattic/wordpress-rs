d9c975c8b53d51185cb678b58e0b970a6340b406
Oguz Kocer <oguz.kocer@automattic.com>
2024-04-11 20:08:11 -0400

    Introduce ApiBaseUrl

--- wp_api/src/endpoint.rs
+++ wp_api/src/endpoint.rs
@@ -4,11 +4,11 @@ use crate::{UserDeleteParams, UserId, UserListParams, UserUpdateParams, WPContex
 
 const WP_JSON_PATH_SEGMENTS: [&str; 3] = ["wp-json", "wp", "v2"];
 
-pub struct ApiEndpoint {
+pub struct ApiBaseUrl {
     base_url: Url,
 }
 
-impl ApiEndpoint {
+impl ApiBaseUrl {
     pub fn new(site_base_url: &str) -> Result<Self, url::ParseError> {
         Url::parse(site_base_url).map(|parsed_url| {
             let base_url = parsed_url
@@ -18,18 +18,14 @@ impl ApiEndpoint {
         })
     }
 
-    pub fn users(&self) -> UsersEndpoint {
-        UsersEndpoint { api_base_url: self }
-    }
-
-    fn append_to_api_base_url(&self, segment: &str) -> Url {
+    fn by_appending(&self, segment: &str) -> Url {
         self.base_url
             .clone()
             .append(segment)
             .expect("api_base_url is already parsed, so this can't result in an error")
     }
 
-    fn extend_api_base_url<I>(&self, segments: I) -> Url
+    fn by_extending<I>(&self, segments: I) -> Url
     where
         I: IntoIterator,
         I::Item: AsRef<str>,
@@ -41,31 +37,47 @@ impl ApiEndpoint {
     }
 }
 
+pub struct ApiEndpoint {
+    api_base_url: ApiBaseUrl,
+}
+
+impl ApiEndpoint {
+    pub fn new(site_base_url: &str) -> Result<Self, url::ParseError> {
+        ApiBaseUrl::new(site_base_url).map(|api_base_url| Self { api_base_url })
+    }
+
+    pub fn users(&self) -> UsersEndpoint {
+        UsersEndpoint {
+            api_base_url: &self.api_base_url,
+        }
+    }
+}
+
 pub struct UsersEndpoint<'a> {
-    api_base_url: &'a ApiEndpoint,
+    api_base_url: &'a ApiBaseUrl,
 }
 
 impl UsersEndpoint<'_> {
     pub fn create(&self) -> Url {
-        self.api_base_url.append_to_api_base_url("users")
+        self.api_base_url.by_appending("users")
     }
 
     pub fn delete(&self, user_id: UserId, params: &UserDeleteParams) -> Url {
         let mut url = self
             .api_base_url
-            .extend_api_base_url(["users", &user_id.to_string()]);
+            .by_extending(["users", &user_id.to_string()]);
         url.query_pairs_mut().extend_pairs(params.query_pairs());
         url
     }
 
     pub fn delete_me(&self, params: &UserDeleteParams) -> Url {
-        let mut url = self.api_base_url.extend_api_base_url(["users", "me"]);
+        let mut url = self.api_base_url.by_extending(["users", "me"]);
         url.query_pairs_mut().extend_pairs(params.query_pairs());
         url
     }
 
     pub fn list(&self, context: WPContext, params: Option<&UserListParams>) -> Url {
-        let mut url = self.api_base_url.append_to_api_base_url("users");
+        let mut url = self.api_base_url.by_appending("users");
         url.query_pairs_mut()
             .append_pair("context", context.as_str());
         if let Some(params) = params {
@@ -77,14 +89,14 @@ impl UsersEndpoint<'_> {
     pub fn retrieve(&self, user_id: UserId, context: WPContext) -> Url {
         let mut url = self
             .api_base_url
-            .extend_api_base_url(["users", &user_id.to_string()]);
+            .by_extending(["users", &user_id.to_string()]);
         url.query_pairs_mut()
             .append_pair("context", context.as_str());
         url
     }
 
     pub fn retrieve_me(&self, context: WPContext) -> Url {
-        let mut url = self.api_base_url.extend_api_base_url(["users", "me"]);
+        let mut url = self.api_base_url.by_extending(["users", "me"]);
         url.query_pairs_mut()
             .append_pair("context", context.as_str());
         url
@@ -92,11 +104,11 @@ impl UsersEndpoint<'_> {
 
     pub fn update(&self, user_id: UserId, params: &UserUpdateParams) -> Url {
         self.api_base_url
-            .extend_api_base_url(["users", &user_id.to_string()])
+            .by_extending(["users", &user_id.to_string()])
     }
 
     pub fn update_me(&self, params: &UserUpdateParams) -> Url {
-        self.api_base_url.extend_api_base_url(["users", "me"])
+        self.api_base_url.by_extending(["users", "me"])
     }
 }
 
@@ -129,14 +141,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn new_site_base_url() {
+    // This test is temporary, just using to test some stuff while working on the implementation
+    fn temporary_test() {
         let base_url = "https://example.com/blog";
-        let site_base_url = WpOrgApiBaseUrl::new(base_url).unwrap();
+        let api_endpoint = ApiEndpoint::new(base_url).unwrap();
         assert_eq!(
             Url::parse(format!("{}/{}", base_url, WP_JSON_PATH_SEGMENTS.join("/")).as_str())
                 .unwrap()
                 .as_str(),
-            site_base_url.api_base_url.as_str()
+            api_endpoint.api_base_url.base_url.as_str()
         );
     }
 }
