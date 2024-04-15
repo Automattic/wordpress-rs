4a9a84f790f465358f689fc0c5711a43efa590b0
Oguz Kocer <oguz.kocer@automattic.com>
2024-04-15 16:38:57 -0400

    Try asserting list users by comparing it to DB

--- wp_networking/tests/test_users.rs
+++ wp_networking/tests/test_users.rs
@@ -1,35 +1,88 @@
-// use base64::prelude::*;
-// use std::fs::read_to_string;
-// use wp_api::{
-//     UserCreateParamsBuilder, UserUpdateParamsBuilder, UserWithEditContext, WPApiError,
-//     WPAuthentication, WPContext, WPNetworkResponse,
-// };
-//
-// use wp_networking::WPNetworking;
-//
-// fn wp_networking() -> WPNetworking {
-//     let file_contents = read_to_string("../test_credentials").unwrap();
-//     let lines: Vec<&str> = file_contents.lines().collect();
-//     let site_url = lines[0];
-//     let auth_base64_token = BASE64_STANDARD.encode(format!("{}:{}", lines[1], lines[2]));
-//
-//     let authentication = WPAuthentication::AuthorizationHeader {
-//         token: auth_base64_token,
-//     };
-//
-//     WPNetworking::new(site_url.into(), authentication)
-// }
-//
-// fn list_users_with_edit_context() -> Vec<UserWithEditContext> {
-//     let user_list_request = wp_networking()
-//         .api_helper
-//         .list_users_request(WPContext::Edit, None);
-//     wp_api::parse_list_users_response_with_edit_context(
-//         &wp_networking().request(user_list_request).unwrap(),
-//     )
-//     .unwrap()
-// }
-//
+use base64::prelude::*;
+use std::fs::read_to_string;
+use wp_api::{UserWithEditContext, WPAuthentication, WPContext};
+
+use wp_networking::WPNetworking;
+
+fn wp_networking() -> WPNetworking {
+    let file_contents = read_to_string("../test_credentials").unwrap();
+    let lines: Vec<&str> = file_contents.lines().collect();
+    let site_url = lines[0];
+    let auth_base64_token = BASE64_STANDARD.encode(format!("{}:{}", lines[1], lines[2]));
+
+    let authentication = WPAuthentication::AuthorizationHeader {
+        token: auth_base64_token,
+    };
+
+    WPNetworking::new(site_url.into(), authentication)
+}
+
+fn list_users_with_edit_context() -> Vec<UserWithEditContext> {
+    let user_list_request = wp_networking()
+        .api_helper
+        .list_users_request(WPContext::Edit, &None);
+    wp_api::parse_list_users_response_with_edit_context(
+        &wp_networking().request(user_list_request).unwrap(),
+    )
+    .unwrap()
+}
+
+#[tokio::test]
+async fn test_list_users() {
+    let users_from_db = fetch_db_users().await.unwrap();
+    let users_from_api = tokio::task::spawn_blocking(move || list_users_with_edit_context())
+        .await
+        .unwrap();
+    users_from_db
+        .iter()
+        .zip(users_from_api.iter())
+        .for_each(|(db_user, api_user)| {
+            assert_eq!(wp_api::UserId(db_user.user_id as i32), api_user.id);
+            assert_eq!(db_user.user_login, api_user.username);
+            assert_eq!(db_user.user_nicename, api_user.slug);
+            assert_eq!(db_user.user_email, api_user.email);
+            assert_eq!(db_user.user_url, api_user.url);
+            assert_eq!(
+                db_user.user_registered,
+                api_user
+                    .registered_date
+                    .parse::<chrono::DateTime<chrono::Utc>>()
+                    .unwrap()
+            );
+            assert_eq!(db_user.display_name, api_user.name);
+        });
+}
+
+use sqlx::{mysql::MySqlConnectOptions, ConnectOptions, MySqlConnection};
+
+#[derive(Debug, sqlx::FromRow)]
+pub struct DbUser {
+    #[sqlx(rename = "ID")]
+    user_id: u64,
+    user_login: String,
+    user_nicename: String,
+    user_email: String,
+    user_url: String,
+    user_registered: chrono::DateTime<chrono::Utc>,
+    display_name: String,
+}
+
+async fn fetch_db_users() -> Result<Vec<DbUser>, sqlx::Error> {
+    let mut conn = db().await?;
+    sqlx::query_as("SELECT * FROM wp_users")
+        .fetch_all(&mut conn)
+        .await
+}
+
+async fn db() -> Result<MySqlConnection, sqlx::Error> {
+    let options = MySqlConnectOptions::new()
+        .host("localhost")
+        .username("wordpress")
+        .password("wordpress")
+        .database("wordpress");
+    MySqlConnectOptions::connect(&options).await
+}
+
 // fn create_test_user() -> (
 //     WPNetworkResponse,
 //     Result<Option<UserWithEditContext>, WPApiError>,
@@ -48,11 +101,6 @@
 //     let created_user =
 //         wp_api::parse_retrieve_user_response_with_edit_context(&user_create_response);
 //     (user_create_response, created_user)
-// }
-//
-// #[test]
-// fn test_list_users() {
-//     println!("User List: {:?}", list_users_with_edit_context());
 // }
 //
 // #[test]
@@ -100,24 +148,3 @@
 //     );
 //     println!("Updated User: {:?}", updated_user);
 // }
-
-use sqlx::{mysql::MySqlConnectOptions, ConnectOptions, MySqlConnection};
-
-#[tokio::test]
-async fn fetch_db_users() {
-    let mut conn = db().await;
-    let rows = sqlx::query("SELECT * FROM wp_users")
-        .fetch_all(&mut conn)
-        .await;
-    println!("{:?}", rows);
-    println!("{:?}", rows.unwrap().len());
-}
-
-async fn db() -> MySqlConnection {
-    let options = MySqlConnectOptions::new()
-        .host("localhost")
-        .username("wordpress")
-        .password("wordpress")
-        .database("wordpress");
-    MySqlConnectOptions::connect(&options).await.unwrap()
-}
