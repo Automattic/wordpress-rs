042f0a178889319fd99ae2a653a176aa1d414034
Oguz Kocer <oguz.kocer@automattic.com>
2024-04-23 15:08:42 -0400

    Extract test_credentials helper for integration tests

--- wp_networking/tests/test_helpers.rs
+++ wp_networking/tests/test_helpers.rs
@@ -9,14 +9,22 @@ pub const FIRST_USER_ID: UserId = UserId(1);
 pub const SECOND_USER_ID: UserId = UserId(2);
 
 pub fn wp_networking() -> AsyncWPNetworking {
-    let file_contents = read_to_string("../test_credentials").unwrap();
-    let lines: Vec<&str> = file_contents.lines().collect();
-    let site_url = lines[0];
-    let auth_base64_token = BASE64_STANDARD.encode(format!("{}:{}", lines[1], lines[2]));
+    let (site_url, username, password) = test_credentials();
+    let auth_base64_token = BASE64_STANDARD.encode(format!("{}:{}", username, password));
 
     let authentication = WPAuthentication::AuthorizationHeader {
         token: auth_base64_token,
     };
 
     AsyncWPNetworking::new(site_url.into(), authentication)
+}
+
+pub fn test_credentials() -> (String, String, String) {
+    let file_contents = read_to_string("../test_credentials").unwrap();
+    let lines: Vec<&str> = file_contents.lines().collect();
+    (
+        lines[0].to_string(),
+        lines[1].to_string(),
+        lines[2].to_string(),
+    )
 }
