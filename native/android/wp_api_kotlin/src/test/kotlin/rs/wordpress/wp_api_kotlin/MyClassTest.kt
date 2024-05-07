cbd97e0404e2850e6bc52edce2907492a2156328
Oguz Kocer <oguz.kocer@automattic.com>
2024-05-07 13:45:27 -0400

    Setup integration tests in wp_api_kotlin

---
+++ native/android/wp_api_kotlin/src/test/kotlin/rs/wordpress/wp_api_kotlin/MyClassTest.kt
@@ -0,0 +1,55 @@
+package rs.wordpress.wp_api_kotlin
+
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import uniffi.wp_api.PostObject
+import uniffi.wp_api.RequestMethod
+import uniffi.wp_api.WpApiException
+import uniffi.wp_api.WpAuthentication
+import uniffi.wp_api.WpRestErrorCode
+import uniffi.wp_api.WpRestErrorWrapper
+import uniffi.wp_api.wpAuthenticationFromUsernameAndPassword
+import kotlin.test.assertFailsWith
+
+private const val TEST_SITE_URL = "http://localhost"
+private const val TEST_ADMIN_USERNAME = "test@example.com"
+private const val TEST_ADMIN_PASSWORD = "iKeooKWpAKsjTrU3xWgqL7HB"
+
+class MyClassTest {
+    private val siteUrl = TEST_SITE_URL
+    private val authentication = wpAuthenticationFromUsernameAndPassword(
+        username = TEST_ADMIN_USERNAME,
+        password = TEST_ADMIN_PASSWORD
+    )
+    private val library = MyClass(siteUrl, authentication)
+
+    @Test
+    fun testBasicPostListRequest() {
+        val request = library.postListRequest()
+        assert(request.method == RequestMethod.GET)
+        assert(request.url == "$siteUrl/wp-json/wp/v2/posts?context=edit&page=1&per_page=10")
+    }
+
+    @Test
+    fun testMakeBasicPostListRequest() {
+        val postListResponse = library.makePostListRequest()
+        val firstPost: PostObject = postListResponse.postList!!.first()
+        assert(firstPost.title?.raw == "Hello world!")
+    }
+
+    @Test
+    fun testPostListRequestForbiddenContext() {
+        val unauthenticatedLibrary =
+            MyClass(siteUrl, WpAuthentication.AuthorizationHeader("invalid_token"))
+        val exception = assertFailsWith<WpApiException.RestException> {
+            unauthenticatedLibrary.makePostListRequest()
+        }
+        val expectedStatusCode: UShort = 401u
+        assertEquals(expectedStatusCode, exception.statusCode)
+        assertEquals(
+            WpRestErrorCode.ForbiddenContext,
+            (exception.restError as WpRestErrorWrapper.Recognized).v1.code
+        )
+    }
+}
