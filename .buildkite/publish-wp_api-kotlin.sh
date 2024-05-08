30e0c1e39c1e2d74d2965ee49b0d7462a404fc92
Oguz Kocer <oguz.kocer@automattic.com>
2024-05-08 14:52:05 -0400

    Implement publishing for jvm and android libraries

---
+++ .buildkite/publish-wp_api-kotlin.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euo pipefail
+
+./gradlew \
+    :wp_api_kotlin:prepareToPublishToS3 "$(prepare_to_publish_to_s3_params)" \
+    :wp_api_kotlin:publish
+
+# Add meta-data for the published version so we can use it in subsequent steps
+buildkite-agent meta-data set "PUBLISHED_WP_API_KOTLIN_VERSION" < ./fluxc-annotations/build/published-version.txt
