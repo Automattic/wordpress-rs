7d9ae71779bc6205b788c06cff9e448be0d207ce
Oguz Kocer <oguzkocer@users.noreply.github.com>
2024-05-10 13:34:57 -0400

    Kotlin module (#81)
    * Add wp_api_kotlin module
    
    * Generate the kotlin bindings in wp_api_kotlin instead of wp_api
    
    * Setup integration tests in wp_api_kotlin
    
    * Refactor wp_api_kotlin/build.gradle as self-contained module
    
    * Use assertEquals in Kotlin tests
    
    * Extract jniLibsPath
    
    * Use JvmTestSuite for kotlin integration tests
    
    * Use test_credentials in jvm integration tests
    
    * Use a singleton for Kotlin test credentials
    
    * Remove unnecessary srcDirs declaration for Kotlin integration tests
    
    * Implement publishing for jvm and android libraries
    
    * Implement kotlin publishing in Buildkite
    
    * Try '$$HOME/.cargo/env' for Android Buildkite agent
    
    * Add executable permission to Kotlin publish Buildkite scripts
    
    * Add ci-toolkit plugin to Android steps and navigate to android directory
    
    * Conditionally add test credentials to wp_api build config fields
    
    * Add separators to Buildkite pipeline for Kotlin publishing
    
    * Remove quotations from prepare_to_publish_to_s3_params
    
    * Use OS dependent extension for generating Kotlin bindings
    
    * Fix the path to read published wp_api_kotlin version
    
    * Add 'setup-rust-android-toolchain' make task and use it for Android publishing
    
    * Add S3 Maven repo to wp_api module
    
    * Minor improvements to Kotlin Buildkite pipeline
    
    * Add missing 'Publish' label to Kotlin Buildkite pipeline step

---
+++ .buildkite/publish-wp_api-kotlin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euo pipefail
+
+cd ./native/android
+./gradlew \
+    :wp_api_kotlin:prepareToPublishToS3 $(prepare_to_publish_to_s3_params) \
+    :wp_api_kotlin:publish
+
+# Add meta-data for the published version so we can use it in subsequent steps
+buildkite-agent meta-data set "PUBLISHED_WP_API_KOTLIN_VERSION" < ./wp_api_kotlin/build/published-version.txt
