c24fe07e52843695f0874f8f600cc46189eaa2b7
Oguz Kocer <oguz.kocer@automattic.com>
2024-05-14 13:59:33 -0400

    Implement list users with embed & view context in Kotlin

--- native/kotlin/api/kotlin/src/main/kotlin/rs/wordpress/api/kotlin/UsersEndpoint.kt
+++ native/kotlin/api/kotlin/src/main/kotlin/rs/wordpress/api/kotlin/UsersEndpoint.kt
@@ -2,7 +2,11 @@ package rs.wordpress.api.kotlin
 
 import uniffi.wp_api.UserListParams
 import uniffi.wp_api.UserWithEditContext
+import uniffi.wp_api.UserWithEmbedContext
+import uniffi.wp_api.UserWithViewContext
 
 interface UsersEndpoint {
     suspend fun listWithEditContext(params: UserListParams?): WpRequestResult<List<UserWithEditContext>>
+    suspend fun listWithEmbedContext(params: UserListParams?): WpRequestResult<List<UserWithEmbedContext>>
+    suspend fun listWithViewContext(params: UserListParams?): WpRequestResult<List<UserWithViewContext>>
 }
--- native/kotlin/api/kotlin/src/main/kotlin/rs/wordpress/api/kotlin/WPUsersEndpoint.kt
+++ native/kotlin/api/kotlin/src/main/kotlin/rs/wordpress/api/kotlin/WPUsersEndpoint.kt
@@ -2,9 +2,13 @@ package rs.wordpress.api.kotlin
 
 import uniffi.wp_api.UserListParams
 import uniffi.wp_api.UserWithEditContext
+import uniffi.wp_api.UserWithEmbedContext
+import uniffi.wp_api.UserWithViewContext
 import uniffi.wp_api.WpApiHelper
 import uniffi.wp_api.WpContext
 import uniffi.wp_api.parseListUsersResponseWithEditContext
+import uniffi.wp_api.parseListUsersResponseWithEmbedContext
+import uniffi.wp_api.parseListUsersResponseWithViewContext
 
 class WPUsersEndpoint(
     private val requestHandler: WpRequestHandler,
@@ -17,5 +21,23 @@ class WPUsersEndpoint(
                 params = params
             ),
             parser = ::parseListUsersResponseWithEditContext
+        )
+
+    override suspend fun listWithEmbedContext(params: UserListParams?): WpRequestResult<List<UserWithEmbedContext>> =
+        requestHandler.execute(
+            request = apiHelper.listUsersRequest(
+                context = WpContext.EMBED,
+                params = params
+            ),
+            parser = ::parseListUsersResponseWithEmbedContext
+        )
+
+    override suspend fun listWithViewContext(params: UserListParams?): WpRequestResult<List<UserWithViewContext>> =
+        requestHandler.execute(
+            request = apiHelper.listUsersRequest(
+                context = WpContext.VIEW,
+                params = params
+            ),
+            parser = ::parseListUsersResponseWithViewContext
         )
 }
