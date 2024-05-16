ef837fb06151104ef350a3b190e762ea14a1fb12
Oguz Kocer <oguz.kocer@automattic.com>
2024-05-16 14:16:12 -0400

    Remove temporary scripts used to test plugins

--- create-plugin.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl --header "Content-Type: application/json" \
-  -u 'test@example.com:ZluvFUYwYOFs5o3u3Th4u3gy' \
-  --request POST \
-  --data '{"slug":"hello-dolly"}' \
-  "http://localhost/wp-json/wp/v2/plugins/"
--- fetch-plugin.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl "http://localhost/wp-json/wp/v2/plugins/hello-dolly\/hello?" -u 'test@example.com:xlehGIszv9cA5XgF9rTfCmpY'
-curl "http://localhost/wp-json/wp/v2/plugins/classic-editor/classic-editor?" -u 'test@example.com:xlehGIszv9cA5XgF9rTfCmpY'
