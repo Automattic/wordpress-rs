a485e4291a15cd3b64b3ce896a16d94ee744fa74
Oguz Kocer <oguz.kocer@automattic.com>
2024-05-29 12:16:13 -0400

    Remove trailing whitespace in WordPressAPITests.swift:12

--- native/swift/Tests/wordpress-api/WordPressAPITests.swift
+++ native/swift/Tests/wordpress-api/WordPressAPITests.swift
@@ -9,7 +9,7 @@ final class WordPressAPITests: XCTestCase {
 
     func testExample() throws {
         let request = try WpRequestBuilder(siteUrl: "https://wordpress.org", authentication: .none)
-            .users().list(context: .view, params: nil) 
+            .users().list(context: .view, params: nil)
         XCTAssertTrue(try XCTUnwrap(request).url.hasPrefix("https://wordpress.org/wp-json/wp/v2/users"))
     }
 }
