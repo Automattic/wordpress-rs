f1808b5c3000e938bfc57a1d89e4e01e22dd2d5a
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2024-05-29 10:03:19 -0600

    Fix tests (#125)

--- native/swift/Tests/wordpress-api/WordPressAPITests.swift
+++ native/swift/Tests/wordpress-api/WordPressAPITests.swift
@@ -10,6 +10,6 @@ final class WordPressAPITests: XCTestCase {
     func testExample() throws {
         let request = try WpRequestBuilder(siteUrl: "https://wordpress.org", authentication: .none)
             .users().list(context: .view, params: nil) 
-        XCTAssertTrue(XCTUnwrap(request).url.hasPrefix("https://wordpress.org/wp-json/wp/v2/users"))
+        XCTAssertTrue(try XCTUnwrap(request).url.hasPrefix("https://wordpress.org/wp-json/wp/v2/users"))
     }
 }
