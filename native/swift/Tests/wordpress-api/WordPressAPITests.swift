368bc4706eed62dc8f9da47177f623c02477d23b
Tony Li <tony.li@automattic.com>
2024-06-05 12:33:38 +1200

    Address SwiftLint issues

--- native/swift/Tests/wordpress-api/WordPressAPITests.swift
+++ native/swift/Tests/wordpress-api/WordPressAPITests.swift
@@ -5,8 +5,14 @@ import WordPressAPI
 final class WordPressAPITests: XCTestCase {
 
     func testExample() async throws {
+        // swiftlint:disable todo
         // TODO: Implement a `RequestExecutor` to stub HTTP requests in unit tests.
-        let api = try WordPressAPI(urlSession: .shared, baseUrl: URL(string: "https://wordpress.org")!, authenticationStategy: .none)
+        // swiftlint:enable todo
+        let api = try WordPressAPI(
+            urlSession: .shared,
+            baseUrl: URL(string: "https://wordpress.org")!,
+            authenticationStategy: .none
+        )
         let users = try await api.users.listWithViewContext(params: nil)
         XCTAssertFalse(users.isEmpty)
     }
