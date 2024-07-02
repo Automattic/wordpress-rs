0fb0d0bc32b9e7d925a659dbfb73c10a2491e337
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2024-07-02 15:57:30 -0600

    Some future-proofing for typed throws

--- native/swift/Sources/wordpress-api/LoginAPI.swift
+++ native/swift/Sources/wordpress-api/LoginAPI.swift
@@ -15,6 +15,17 @@ public class WordPressLoginClient {
 
     public func discoverLoginUrl(for proposedSiteUrl: String) async throws -> UrlDiscoverySuccess {
         let client = UniffiWpLoginClient(requestExecutor: self.urlSession)
-        return try await client.apiDiscovery(siteUrl: proposedSiteUrl)
+
+        do {
+            return try await client.apiDiscovery(siteUrl: proposedSiteUrl)
+        } catch let err {
+            guard let discoveryError = err as? UrlDiscoveryError else {
+                throw err
+            }
+
+            // TODO: Once we adopt Swift 6 this can be a typed throw
+            throw discoveryError
+        }
+
     }
 }
