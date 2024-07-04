20edfec9f40900f2a525b7baf3f0a640ada81190
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2024-07-03 21:00:07 -0600

    lintfix

--- native/swift/Tests/wordpress-api/Foundation+ExtensionsTests.swift
+++ native/swift/Tests/wordpress-api/Foundation+ExtensionsTests.swift
@@ -2,7 +2,7 @@ import XCTest
 import WordPressAPI
 
 final class FoundationExtensionsTests: XCTestCase {
-    
+
     func testWordPressDateTimeParsing() throws {
         XCTAssertNotNil(Date.fromWordPressDate("2024-07-04T01:49:37"))
     }
