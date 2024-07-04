0c589381b351f6bef6e9b1f76092d9e1273558c4
Oguz Kocer <oguz.kocer@automattic.com>
2024-07-04 15:35:23 -0400

    Adds separate Uniffi request builder & client

--- wp_api/src/api_client.rs
+++ wp_api/src/api_client.rs
@@ -13,15 +13,40 @@ use crate::{ParsedUrl, WpAuthentication};
 use std::sync::Arc;
 
 #[derive(Debug, uniffi::Object)]
+pub struct UniffiWpApiRequestBuilder {
+    inner: WpApiRequestBuilder,
+}
+
+#[uniffi::export]
+impl UniffiWpApiRequestBuilder {
+    #[uniffi::constructor]
+    pub fn new(site_url: Arc<ParsedUrl>, authentication: WpAuthentication) -> Self {
+        Self {
+            inner: WpApiRequestBuilder::new(site_url, authentication),
+        }
+    }
+
+    pub fn application_passwords(&self) -> Arc<ApplicationPasswordsRequestBuilder> {
+        self.inner.application_passwords()
+    }
+
+    pub fn users(&self) -> Arc<UsersRequestBuilder> {
+        self.inner.users()
+    }
+
+    pub fn plugins(&self) -> Arc<PluginsRequestBuilder> {
+        self.inner.plugins()
+    }
+}
+
+#[derive(Debug)]
 pub struct WpApiRequestBuilder {
     application_passwords: Arc<ApplicationPasswordsRequestBuilder>,
     users: Arc<UsersRequestBuilder>,
     plugins: Arc<PluginsRequestBuilder>,
 }
 
-#[uniffi::export]
 impl WpApiRequestBuilder {
-    #[uniffi::constructor]
     pub fn new(site_url: Arc<ParsedUrl>, authentication: WpAuthentication) -> Self {
         let api_base_url: Arc<ApiBaseUrl> = Arc::new(site_url.inner.clone().into());
 
@@ -51,15 +76,44 @@ impl WpApiRequestBuilder {
 }
 
 #[derive(Debug, uniffi::Object)]
+pub struct UniffiWpApiClient {
+    inner: WpApiClient,
+}
+
+#[uniffi::export]
+impl UniffiWpApiClient {
+    #[uniffi::constructor]
+    pub fn new(
+        site_url: Arc<ParsedUrl>,
+        authentication: WpAuthentication,
+        request_executor: Arc<dyn RequestExecutor>,
+    ) -> Self {
+        Self {
+            inner: WpApiClient::new(site_url, authentication, request_executor),
+        }
+    }
+
+    pub fn application_passwords(&self) -> Arc<ApplicationPasswordsRequestExecutor> {
+        self.inner.application_passwords()
+    }
+
+    pub fn users(&self) -> Arc<UsersRequestExecutor> {
+        self.inner.users()
+    }
+
+    pub fn plugins(&self) -> Arc<PluginsRequestExecutor> {
+        self.inner.plugins()
+    }
+}
+
+#[derive(Debug)]
 pub struct WpApiClient {
     application_passwords: Arc<ApplicationPasswordsRequestExecutor>,
     users: Arc<UsersRequestExecutor>,
     plugins: Arc<PluginsRequestExecutor>,
 }
 
-#[uniffi::export]
 impl WpApiClient {
-    #[uniffi::constructor]
     pub fn new(
         site_url: Arc<ParsedUrl>,
         authentication: WpAuthentication,
