f06f5a196d8ae15e0c47152d7cfc8145416ce717
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2024-07-25 13:59:59 -0600

    Temp fix for test_login_immut

--- scripts/docker-integration-tests.sh
+++ scripts/docker-integration-tests.sh
@@ -3,7 +3,7 @@
 set -e
 
 apt update
-apt install -y default-mysql-client
+apt install -y default-mysql-client jq
 
 ## Create Database Backup (if needed)
 backup=/app/.wordpress/wp-content/dump.sql
@@ -15,4 +15,4 @@ fi
 cp -R /app/.wordpress/wp-content/plugins /app/.wordpress/wp-content/plugins-backup
 
 ## Run the integration tests
-cargo test --no-fail-fast
+cargo test --no-fail-fast -p wp_api_integration_tests
