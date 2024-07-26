218ad93ff189633b17ce3de707d1df5c48450aa4
Oguz Kocer <oguz.kocer@automattic.com>
2024-07-26 17:17:28 -0400

    Implement update_site_settings language & site_logo

--- wp_api_integration_tests/src/wp_cli.rs
+++ wp_api_integration_tests/src/wp_cli.rs
@@ -24,26 +24,19 @@ pub struct WpCliSiteSettings {
     pub date_format: Option<String>,
     pub time_format: Option<String>,
     pub start_of_week: Option<String>,
-    //pub start_of_week: Option<u64>,
+    #[serde(rename = "WPLANG")]
+    pub language: Option<String>,
     pub use_smilies: Option<String>,
-    //pub use_smilies: Option<bool>,
     pub default_category: Option<String>,
-    //pub default_category: Option<u64>,
     pub default_post_format: Option<String>,
     pub posts_per_page: Option<String>,
-    //pub posts_per_page: Option<u64>,
     pub show_on_front: Option<String>,
     pub page_on_front: Option<String>,
-    //pub page_on_front: Option<u64>,
     pub page_for_posts: Option<String>,
-    //pub page_for_posts: Option<u64>,
     pub default_ping_status: Option<String>,
     pub default_comment_status: Option<String>,
+    pub site_logo: Option<String>,
     pub site_icon: Option<String>,
-    //pub site_icon: Option<u64>,
-    // Is not part of `wp option list`
-    // pub language: Option<String>,
-    // pub site_logo: Option<u64>,
 }
 
 impl WpCliSiteSettings {
