21929ae1ea766dc67c7b2a0930844601952d3873
Oguz Kocer <oguz.kocer@automattic.com>
2024-07-29 17:13:17 -0400

    Add support for contextual HashMap field to `WPContextual`

--- wp_contextual/tests/basic_wp_contextual_field.rs
+++ wp_contextual/tests/basic_wp_contextual_field.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use wp_contextual::WpContextual;
 
 #[derive(WpContextual)]
@@ -5,6 +6,12 @@ pub struct SparseFoo {
     #[WpContext(edit)]
     #[WpContextualField]
     pub bar: Option<SparseBar>,
+    #[WpContext(edit)]
+    #[WpContextualField]
+    pub vec_bar: Option<Vec<SparseBar>>,
+    #[WpContext(edit)]
+    #[WpContextualField]
+    pub hash_map_bar: Option<std::collections::HashMap<String, SparseBar>>,
 }
 
 #[derive(Debug, serde::Serialize, serde::Deserialize, uniffi::Record, WpContextual)]
@@ -16,9 +23,16 @@ pub struct SparseBar {
 fn main() {
     let _ = FooWithEditContext {
         bar: BarWithEditContext { baz: 0 },
+        vec_bar: vec![BarWithEditContext { baz: 1 }],
+        hash_map_bar: HashMap::from([("bar_2".to_string(), BarWithEditContext { baz: 2 })]),
     };
     let _ = SparseFooWithEditContext {
         bar: Some(SparseBarWithEditContext { baz: Some(0) }),
+        vec_bar: Some(vec![SparseBarWithEditContext { baz: Some(1) }]),
+        hash_map_bar: Some(HashMap::from([(
+            "bar_2".to_string(),
+            SparseBarWithEditContext { baz: Some(2) },
+        )])),
     };
     let bar_field = SparseFooFieldWithEditContext::Bar;
     assert_eq!(bar_field.as_field_name(), "bar");
