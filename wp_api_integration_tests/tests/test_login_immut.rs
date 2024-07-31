6b8c81aca4545db1c30a31f205f3a73d8c2237c6
Oguz Kocer <oguz.kocer@automattic.com>
2024-07-31 16:54:04 -0400

    Use `automatticwidgets.wpcomstaging.com` for auto-discovery tests

--- wp_api_integration_tests/tests/test_login_immut.rs
+++ wp_api_integration_tests/tests/test_login_immut.rs
@@ -5,25 +5,36 @@ use wp_api::login::WpLoginClient;
 use wp_api_integration_tests::{AssertResponse, AsyncWpNetworking};
 
 const LOCALHOST_AUTH_URL: &str = "http://localhost/wp-admin/authorize-application.php";
-const ORCHESTREMETROPOLITAIN_AUTH_URL: &str =
-    "https://orchestremetropolitain.com/wp-admin/authorize-application.php";
+const AUTOMATTIC_WIDGETS_AUTH_URL: &str =
+    "https://automatticwidgets.wpcomstaging.com/wp-admin/authorize-application.php";
 
 #[rstest]
 #[case("http://localhost", LOCALHOST_AUTH_URL)]
-#[case("http://localhost/wp-json", LOCALHOST_AUTH_URL)]
-#[case("http://localhost/wp-admin.php", LOCALHOST_AUTH_URL)]
 #[case("http://localhost/wp-admin", LOCALHOST_AUTH_URL)]
+#[case("http://localhost/wp-admin.php", LOCALHOST_AUTH_URL)]
 #[case("http://localhost/wp-admin/", LOCALHOST_AUTH_URL)]
-#[case("orchestremetropolitain.com/wp-json", ORCHESTREMETROPOLITAIN_AUTH_URL)]
-#[case("https://orchestremetropolitain.com", ORCHESTREMETROPOLITAIN_AUTH_URL)]
+#[case("http://localhost/wp-json", LOCALHOST_AUTH_URL)]
 #[case(
-    "https://orchestremetropolitain.com/fr/",
-    ORCHESTREMETROPOLITAIN_AUTH_URL
+    "https://automatticwidgets.wpcomstaging.com/",
+    AUTOMATTIC_WIDGETS_AUTH_URL
 )]
 #[case(
-    "https://orchestremetropolitain.com/wp-json",
-    ORCHESTREMETROPOLITAIN_AUTH_URL
+    "https://automatticwidgets.wpcomstaging.com/wp-admin",
+    AUTOMATTIC_WIDGETS_AUTH_URL
 )]
+#[case(
+    "https://automatticwidgets.wpcomstaging.com/wp-admin.php",
+    AUTOMATTIC_WIDGETS_AUTH_URL
+)]
+#[case(
+    "https://automatticwidgets.wpcomstaging.com/wp-admin/",
+    AUTOMATTIC_WIDGETS_AUTH_URL
+)]
+#[case(
+    "https://automatticwidgets.wpcomstaging.com/wp-json",
+    AUTOMATTIC_WIDGETS_AUTH_URL
+)]
+#[case("automatticwidgets.wpcomstaging.com/ ", AUTOMATTIC_WIDGETS_AUTH_URL)]
 #[tokio::test]
 #[serial]
 async fn test_login_flow(#[case] site_url: &str, #[case] expected_auth_url: &str) {
