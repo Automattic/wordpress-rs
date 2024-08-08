e51ae2b49846120e6e3ee23e3f33b2a90d57b7d1
Oguz Kocer <oguz.kocer@automattic.com>
2024-08-08 17:46:59 -0400

    Report Kotlin integration test results to Buildkite

---
+++ .buildkite/commands/run-kotlin-integration-tests.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -eu
+
+test_results_dir="./native/kotlin/api/kotlin/build/test-results/integrationTest"
+test_log_dir="${test_results_dir}/*.xml"
+results_file="$test_results_dir/merged-test-results.xml"
+
+# Give read/write permissions to `./` for all users
+chmod -R a+rw ./
+
+echo "--- :docker: Setting up Test Server"
+make test-server
+
+echo "--- 🧪 Running Kotlin Integration Tests"
+make test-kotlin-integration
+
+echo "--- 🧪 Print test results directory contents"
+ls -l ./native/kotlin/api/kotlin/build/test-results/integrationTest
+
+echo "--- 🚦 Report Tests Status"
+merge_junit_reports -d ${test_log_dir%/*} -o $results_file
+
+echo "--- 🧪 Copying test logs for test collector"
+mkdir buildkite-test-analytics
+cp $results_file buildkite-test-analytics
