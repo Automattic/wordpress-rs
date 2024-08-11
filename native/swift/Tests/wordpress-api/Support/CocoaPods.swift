32253c2fc1d69b45ee7a2950233cc59b2ce357fc
Tony Li <tony.li@automattic.com>
2024-08-12 08:40:13 +1200

    Remove a debugging code

--- native/swift/Tests/wordpress-api/Support/CocoaPods.swift
+++ native/swift/Tests/wordpress-api/Support/CocoaPods.swift
@@ -7,7 +7,6 @@ private class BundleFinder {}
 extension Bundle {
     static var module: Bundle {
         let url = Bundle(for: BundleFinder.self).url(forResource: "Resources", withExtension: "bundle")!
-        try? Bundle(for: BundleFinder.self).bundleURL.absoluteString.write(toFile: "/Users/tonyli/Projects/wordpress-rs/file.path", atomically: true, encoding: .utf8)
         return Bundle(url: url)!
     }
 }
