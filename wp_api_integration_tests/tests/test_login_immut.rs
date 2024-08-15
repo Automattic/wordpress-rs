59e02787270efc7b4bd126628cb7740cdd0c060b
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2024-08-14 18:37:29 -0600

    Add a failing test for no authentication schemes

--- wp_api_integration_tests/tests/test_login_immut.rs
+++ wp_api_integration_tests/tests/test_login_immut.rs
@@ -2,11 +2,12 @@ use rstest::rstest;
 use serial_test::serial;
 use std::sync::Arc;
 use wp_api::login::WpLoginClient;
-use wp_api_integration_tests::{AssertResponse, AsyncWpNetworking};
+use wp_api_integration_tests::{AssertError, AssertResponse, AsyncWpNetworking};
 
 const LOCALHOST_AUTH_URL: &str = "http://localhost/wp-admin/authorize-application.php";
 const AUTOMATTIC_WIDGETS_AUTH_URL: &str =
     "https://automatticwidgets.wpcomstaging.com/wp-admin/authorize-application.php";
+const VANILLA_WP_SITE_URL: &str = "https://vanilla.wpmt.co/wp-admin/authorize-application.php";
 
 #[rstest]
 #[case("http://localhost", LOCALHOST_AUTH_URL)]
@@ -35,6 +36,9 @@ const AUTOMATTIC_WIDGETS_AUTH_URL: &str =
     AUTOMATTIC_WIDGETS_AUTH_URL
 )]
 #[case("automatticwidgets.wpcomstaging.com/ ", AUTOMATTIC_WIDGETS_AUTH_URL)]
+#[case("vanilla.wpmt.co", VANILLA_WP_SITE_URL)]
+#[case("http://vanilla.wpmt.co", VANILLA_WP_SITE_URL)]
+#[case("https://optional-https.wpmt.co", "https://optional-https.wpmt.co/wp-admin/authorize-application.php")]
 #[tokio::test]
 #[serial]
 async fn test_login_flow(#[case] site_url: &str, #[case] expected_auth_url: &str) {
@@ -49,4 +53,19 @@ async fn test_login_flow(#[case] site_url: &str, #[case] expected_auth_url: &str
             .find_application_passwords_authentication_url(),
         Some(expected_auth_url.to_string())
     );
+}
+
+#[rstest]
+#[case("http://optional-https.wpmt.co")]
+#[tokio::test]
+#[serial]
+async fn test_login_flow_for_empty_authentication_schemes(#[case] site_url: &str) {
+    let client = WpLoginClient::new(Arc::new(AsyncWpNetworking::default()));
+    let url_discovery_error = client
+        .api_discovery(site_url.to_string())
+        .await
+        .assert_error();
+
+    // TODO: This test should validate that there are no authentication schemes available
+    assert_eq!(true, false)
 }
