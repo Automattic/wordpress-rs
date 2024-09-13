3721cbb2c8723eb80861530d8b48f28926f9935f
Oguz Kocer <oguzkocer@users.noreply.github.com>
2024-09-13 15:39:09 -0400

    Allow using `OkHttpClient`s in Kotlin wrapper (#309)

--- native/kotlin/api/kotlin/src/main/kotlin/rs/wordpress/api/kotlin/WpRequestExecutor.kt
+++ native/kotlin/api/kotlin/src/main/kotlin/rs/wordpress/api/kotlin/WpRequestExecutor.kt
@@ -11,9 +11,10 @@ import uniffi.wp_api.WpNetworkHeaderMap
 import uniffi.wp_api.WpNetworkRequest
 import uniffi.wp_api.WpNetworkResponse
 
-internal class WpRequestExecutor(private val dispatcher: CoroutineDispatcher = Dispatchers.IO) :
-    RequestExecutor {
-    private val client = OkHttpClient()
+class WpRequestExecutor(
+    private val okHttpClient: OkHttpClient = OkHttpClient(),
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
+) : RequestExecutor {
 
     override suspend fun execute(request: WpNetworkRequest): WpNetworkResponse =
         withContext(dispatcher) {
@@ -28,7 +29,7 @@ internal class WpRequestExecutor(private val dispatcher: CoroutineDispatcher = D
                 }
             }
 
-            client.newCall(requestBuilder.build()).execute().use { response ->
+            okHttpClient.newCall(requestBuilder.build()).execute().use { response ->
                 return@withContext WpNetworkResponse(
                     body = response.body?.bytes() ?: ByteArray(0),
                     statusCode = response.code.toUShort(),
