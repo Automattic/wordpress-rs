7baa7257923dc822da3a8c94c53812a1447be712
Oguz Kocer <oguz.kocer@automattic.com>
2024-10-09 18:21:20 -0400

    Allow error type and request executor to be configured for `#[derive(WpDerivedRequest)]`

--- wp_derive_request_builder/src/lib.rs
+++ wp_derive_request_builder/src/lib.rs
@@ -1,3 +1,6 @@
+use std::{env, fs};
+
+use generate::CrateConfig;
 use proc_macro::TokenStream;
 use syn::parse_macro_input;
 
@@ -5,16 +8,28 @@ mod generate;
 mod parse;
 mod variant_attr;
 
-#[proc_macro_derive(
-    WpDerivedRequest,
-    attributes(SparseField, Namespace, contextual_get, delete, get, post)
-)]
+#[proc_macro_derive(WpDerivedRequest, attributes(contextual_get, delete, get, post))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let parsed_enum = parse_macro_input!(input as parse::ParsedEnum);
 
     if cfg!(feature = "generate_request_builder") {
-        generate::generate_types(&parsed_enum).into()
+        let crate_config = read_crate_config();
+        generate::generate_types(&parsed_enum, &crate_config).into()
     } else {
         TokenStream::new()
     }
+}
+
+fn read_crate_config() -> CrateConfig {
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR")
+        .expect("Crate config can't be found without the `CARGO_MANIFEST_DIR` environment varible");
+    let file_path = format!("{}/wp_derived_request.toml", manifest_dir);
+    let contents = match fs::read_to_string(&file_path) {
+        Ok(c) => toml::from_str(c.as_str())
+            .unwrap_or_else(|e| panic!("'{}' is not formatted correctly:\n{:#?}", file_path, e)),
+        Err(_) => {
+            panic!("{} is missing", file_path);
+        }
+    };
+    contents
 }
--- wp_derive_request_builder/tests/pass/external_outer_attr.rs
+++
@@ -1,8 +0,0 @@
-#[derive(wp_derive_request_builder::WpDerivedRequest, serde::Serialize)]
-#[serde(deny_unknown_fields)]
-enum UsersRequest {
-    #[contextual_get(url = "/users", params = &UserListParams, output = Vec<SparseUser>)]
-    List,
-}
-
-fn main() {}
