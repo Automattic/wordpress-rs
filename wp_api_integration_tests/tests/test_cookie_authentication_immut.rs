06ef60d26e9745e0fa70f8f2bdc30ff5426980d3
Tony Li <tony.li@automattic.com>
2024-10-23 11:09:04 +1300

    Add an unit test to verify nonce is reused across multiple requests

--- wp_api_integration_tests/tests/test_cookie_authentication_immut.rs
+++ wp_api_integration_tests/tests/test_cookie_authentication_immut.rs
@@ -1,8 +1,58 @@
-use wp_api::{WpApiClient, WpAuthentication, WpLoginCredentials};
-
+use futures::lock::Mutex;
+use std::sync::Arc;
+use wp_api::RequestExecutionError;
+use wp_api::{
+    request::{RequestExecutor, WpNetworkRequest, WpNetworkResponse},
+    WpApiClient, WpAuthentication, WpLoginCredentials,
+};
 use wp_api_integration_tests::*;
 
 use serial_test::serial;
+
+#[derive(Debug)]
+struct TrackRequestExecutor<T: RequestExecutor> {
+    requests: Mutex<Vec<Arc<WpNetworkRequest>>>,
+    inner: T,
+}
+
+impl<T: RequestExecutor> TrackRequestExecutor<T> {
+    fn new(request_executor: T) -> Self {
+        Self {
+            requests: vec![].into(),
+            inner: request_executor,
+        }
+    }
+
+    async fn wp_login_requests_count(&self) -> usize {
+        self.requests
+            .lock()
+            .await
+            .iter()
+            .filter(|req| req.url().0.contains("/wp-login.php"))
+            .count()
+    }
+
+    async fn rest_nonce_requests_count(&self) -> usize {
+        self.requests
+            .lock()
+            .await
+            .iter()
+            .filter(|req| req.url().0.contains("action=rest-nonce"))
+            .count()
+    }
+}
+
+#[async_trait::async_trait]
+impl<T: RequestExecutor> RequestExecutor for TrackRequestExecutor<T> {
+    async fn execute(
+        &self,
+        request: Arc<WpNetworkRequest>,
+    ) -> Result<WpNetworkResponse, RequestExecutionError> {
+        self.requests.lock().await.push(request.clone());
+
+        self.inner.execute(request).await
+    }
+}
 
 #[tokio::test]
 #[serial]
@@ -67,4 +117,29 @@ async fn test_fail_without_cookie_store() {
         .retrieve_me_with_edit_context()
         .await
         .assert_wp_error(wp_api::WpErrorCode::Unauthorized);
+}
+
+#[tokio::test]
+#[serial_test::parallel]
+async fn test_nonce_is_reused_across_requests() {
+    let login = WpLoginCredentials {
+        username: TestCredentials::instance().admin_username.to_string(),
+        password: TestCredentials::instance()
+            .admin_account_password
+            .to_string(),
+    };
+    let request_executor = TrackRequestExecutor::new(AsyncWpNetworking::with_cookie_store());
+    let request_executor = std::sync::Arc::new(request_executor);
+    let request_executor_clone = request_executor.clone();
+    let client = WpApiClient::new(
+        test_site_url(),
+        WpAuthentication::UserAccount { login },
+        request_executor,
+    );
+
+    futures::future::join_all((0..100).map(|_| client.users().retrieve_me_with_edit_context()))
+        .await;
+
+    assert_eq!(request_executor_clone.wp_login_requests_count().await, 1);
+    assert_eq!(request_executor_clone.rest_nonce_requests_count().await, 1);
 }
