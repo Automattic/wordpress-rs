1060a83fc900b53a20f847ede08b70c49aff32e0
Oguz Kocer <oguz.kocer@automattic.com>
2025-01-17 17:57:54 -0500

    Add a test case for parsing []

--- wp_api_integration_tests/tests/test_themes_immut.rs
+++ wp_api_integration_tests/tests/test_themes_immut.rs
@@ -12,6 +12,19 @@ use wp_api_integration_tests::{
 };
 
 #[tokio::test]
+#[parallel]
+async fn filter_theme_supports() {
+    api_client()
+        .themes()
+        .filter_list_with_edit_context(
+            &ThemeListParams::default(),
+            &[SparseThemeFieldWithEditContext::ThemeSupports],
+        )
+        .await
+        .assert_response();
+}
+
+#[tokio::test]
 #[apply(list_cases)]
 #[parallel]
 async fn list_with_edit_context(#[case] params: ThemeListParams) {
