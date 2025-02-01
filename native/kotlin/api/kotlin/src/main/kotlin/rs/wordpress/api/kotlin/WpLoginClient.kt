5ab9c30035999753d6cf8d4fd51ea45c4dbe7b70
Oguz Kocer <oguzkocer@users.noreply.github.com>
2025-01-31 19:25:55 -0500

    Address Kotlin breaking changes in api discovery and RequestExecutionError (#522)

--- native/kotlin/api/kotlin/src/main/kotlin/rs/wordpress/api/kotlin/WpLoginClient.kt
+++ native/kotlin/api/kotlin/src/main/kotlin/rs/wordpress/api/kotlin/WpLoginClient.kt
@@ -3,10 +3,9 @@ package rs.wordpress.api.kotlin
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import uniffi.wp_api.AutoDiscoveryUniffiResult
 import uniffi.wp_api.RequestExecutor
 import uniffi.wp_api.UniffiWpLoginClient
-import uniffi.wp_api.UrlDiscoveryException
-import uniffi.wp_api.UrlDiscoverySuccess
 
 class WpLoginClient(
     private val requestExecutor: RequestExecutor = WpRequestExecutor(),
@@ -16,11 +15,7 @@ class WpLoginClient(
         UniffiWpLoginClient(requestExecutor)
     }
 
-    suspend fun apiDiscovery(siteUrl: String): Result<UrlDiscoverySuccess> = withContext(dispatcher) {
-        try {
-            Result.success(internalClient.apiDiscovery(siteUrl))
-        } catch (e: UrlDiscoveryException) {
-            Result.failure(e)
-        }
+    suspend fun apiDiscovery(siteUrl: String): AutoDiscoveryUniffiResult = withContext(dispatcher) {
+        internalClient.apiDiscovery(siteUrl)
     }
 }
