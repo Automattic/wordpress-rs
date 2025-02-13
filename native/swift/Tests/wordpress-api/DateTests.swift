b441d9237898a61ea08b40ec4ac1e0c71eebf9ab
Tony Li <tony.li@automattic.com>
2025-02-14 11:06:26 +1300

    Convert WpGmtDateTime to Foundation.Date

---
+++ native/swift/Tests/wordpress-api/DateTests.swift
@@ -0,0 +1,22 @@
+import Foundation
+import Testing
+import WordPressAPIInternal
+
+struct DateTests {
+    var testDate: Date {
+        // This is the time that's hard-coded in the Rust library.
+        let str = "2020-08-14T15:00:00+02:00"
+        return ISO8601DateFormatter().date(from: str)!
+    }
+
+    @Test
+    func swiftToRust() {
+        assertDateIsConvertedFromNativeToRustCorrectly(date: testDate)
+    }
+
+    @Test
+    func rustToSwift() {
+        let date = assertionExampleDateThatCanBeUsedToVerifyConversionBetweenRustAndNative()
+        #expect(testDate == date)
+    }
+}
