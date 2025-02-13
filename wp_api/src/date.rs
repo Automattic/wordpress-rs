00b4d03d3f7ad8477aad9272ec4c470103deffa9
Oguz Kocer <oguz.kocer@automattic.com>
2025-02-13 00:01:58 -0500

    Add tests to verify date conversion between Rust and Kotlin works correctly

--- wp_api/src/date.rs
+++ wp_api/src/date.rs
@@ -28,3 +28,45 @@ uniffi::custom_type!(WpGmtDateTime, i64, {
     lower: |date_time| date_time.0.timestamp(),
     try_lift: |seconds| Ok(WpGmtDateTime::from_timestamp(seconds)),
 });
+
+// Assertion functions that should only be used by the native test suite
+// These are hidden from the Rust public API, but will be visible/usable in the generated bindings
+mod native_test_helper {
+    use super::WpGmtDateTime;
+    use chrono::offset::FixedOffset;
+
+    const OFFSET_FOR_EXAMPLE_DATE: i32 = 60 * 60 * 2;
+    const EXAMPLE_DATE: &str = "2020-08-14T15:00:00+02:00";
+
+    #[uniffi::export]
+    fn assertion_example_date_that_can_be_used_to_verify_conversion_between_rust_and_native(
+    ) -> WpGmtDateTime {
+        EXAMPLE_DATE
+            .parse::<WpGmtDateTime>()
+            .expect("Example date is parseable")
+    }
+
+    #[uniffi::export]
+    fn assert_date_is_converted_from_native_to_rust_correctly(date: WpGmtDateTime) {
+        assert_eq!(
+            date.0
+                .with_timezone(
+                    &FixedOffset::east_opt(OFFSET_FOR_EXAMPLE_DATE)
+                        .expect("Example offset is valid")
+                )
+                .to_rfc3339(),
+            EXAMPLE_DATE
+        );
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_that_helper_functions_work_correctly() {
+            let date = assertion_example_date_that_can_be_used_to_verify_conversion_between_rust_and_native();
+            assert_date_is_converted_from_native_to_rust_correctly(date);
+        }
+    }
+}
