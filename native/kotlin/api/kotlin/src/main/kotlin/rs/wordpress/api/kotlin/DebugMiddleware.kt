3c5d513c19e593a0e8f116e569f143653e7d051d
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2025-02-27 18:20:03 -0700

    Use Middleware for Kotlin

---
+++ native/kotlin/api/kotlin/src/main/kotlin/rs/wordpress/api/kotlin/DebugMiddleware.kt
@@ -0,0 +1,20 @@
+package rs.wordpress.api.kotlin
+import uniffi.wp_api.RequestExecutor
+import uniffi.wp_api.WpApiMiddleware
+import uniffi.wp_api.WpNetworkRequest
+import uniffi.wp_api.WpNetworkResponse
+
+// Used in a middleware pipeline to print request URLs to the log
+class DebugMiddleware : WpApiMiddleware {
+    override suspend fun process(
+        requestExecutor: RequestExecutor,
+        response: WpNetworkResponse,
+        request: WpNetworkRequest
+    ): WpNetworkResponse {
+        println("Request: ${request.url()}")
+        println("Response:")
+        println("\tStatus Code: ${response.statusCode}")
+        println("\tHeaders: ${response.headerMap}")
+        return response
+    }
+}
--- native/kotlin/api/kotlin/src/main/kotlin/rs/wordpress/api/kotlin/WpLoginClient.kt
+++ native/kotlin/api/kotlin/src/main/kotlin/rs/wordpress/api/kotlin/WpLoginClient.kt
@@ -6,14 +6,16 @@ import kotlinx.coroutines.withContext
 import uniffi.wp_api.AutoDiscoveryUniffiResult
 import uniffi.wp_api.RequestExecutor
 import uniffi.wp_api.UniffiWpLoginClient
+import uniffi.wp_api.WpApiMiddlewarePipeline
+import uniffi.wp_api.defaultMiddlewarePipeline
 
 class WpLoginClient(
-    private val requestExecutor: RequestExecutor = WpRequestExecutor(),
+    requestExecutor: RequestExecutor = WpRequestExecutor(),
+    middlewarePipeline: WpApiMiddlewarePipeline = defaultMiddlewarePipeline(),
     private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) {
-    private val internalClient by lazy {
-        UniffiWpLoginClient(requestExecutor)
-    }
+    private val internalClient: UniffiWpLoginClient =
+        UniffiWpLoginClient(requestExecutor, middlewarePipeline)
 
     suspend fun apiDiscovery(siteUrl: String): AutoDiscoveryUniffiResult = withContext(dispatcher) {
         internalClient.apiDiscovery(siteUrl)
