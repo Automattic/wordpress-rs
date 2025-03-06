657c9898de8586a61a1c6efb3c83de912096a23b
Tony Li <tony.li@automattic.com>
2025-03-07 11:46:19 +1300

    Fix a swiftlint issue

--- native/swift/Tests/wordpress-api/LocalizationTests.swift
+++ native/swift/Tests/wordpress-api/LocalizationTests.swift
@@ -7,7 +7,7 @@ class LocalizationTests {
     @Test
     func testParsingError() {
         do {
-            let _ = try ParsedUrl.parse(input: "not-url")
+            _ = try ParsedUrl.parse(input: "not-url")
             Issue.record("Got an unexpected successful result")
         } catch {
             #expect(error.localizedDescription == "URL is invalid")
