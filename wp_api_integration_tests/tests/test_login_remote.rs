b19d26fd4b3f8aed0038675746989fbc502cf09f
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2025-04-14 12:51:46 -0600

    Pin a working version Hickory
    Fixes issues with DNS resolution errors not being detected

--- wp_api_integration_tests/tests/test_login_remote.rs
+++ wp_api_integration_tests/tests/test_login_remote.rs
@@ -302,12 +302,21 @@ async fn login_spec_15_wordpress_heavy_rate_limiting_that_never_succeeds() {
 #[parallel]
 async fn login_spec_16_invalid_url() {
     // Spec Example 16
-    assert!(matches!(
-        login_err("https://valid-looking-url-but-not-actually.foo")
-            .await
-            .to_find_api_root_failure(),
-        FindApiRootFailure::FetchHomepage { .. }
-    ));
+
+    let request_execution_error_reason =
+    login_err("https://valid-looking-url-but-not-actually.foo")
+        .await
+        .to_fetch_home_page_reason();
+
+        assert!(
+            matches!(
+                request_execution_error_reason,
+                RequestExecutionErrorReason::NonExistentSiteError { .. }
+            ),
+            "Expected RequestExecutionErrorReason::NonExistentSiteError, got: {:?}",
+            request_execution_error_reason
+        );
+    
 }
 
 #[tokio::test]
