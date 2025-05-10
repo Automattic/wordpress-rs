c70de59b1fd31d52577a0b0335af7688d2826484
Oguz Kocer <oguz.kocer@automattic.com>
2025-05-09 20:02:01 -0400

    Use api discovery result in Kotlin example app

--- native/kotlin/example/composeApp/src/commonMain/kotlin/rs/wordpress/example/shared/domain/AuthenticatedSite.kt
+++ native/kotlin/example/composeApp/src/commonMain/kotlin/rs/wordpress/example/shared/domain/AuthenticatedSite.kt
@@ -2,4 +2,4 @@ package rs.wordpress.example.shared.domain
 
 import uniffi.wp_api.ParsedUrl
 
-data class AuthenticatedSite(val name: String, val url: ParsedUrl)
+data class AuthenticatedSite(val name: String, val apiRootUrl: ParsedUrl)
