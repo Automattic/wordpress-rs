e022478ba7688903d662737b01c8bd692c835c6c
Ian Maia <ian.maia@automattic.com>
2025-06-17 18:00:44 +0200

    Update sync script to only push at the end

--- .buildkite/commands/sync-localization.sh
+++ .buildkite/commands/sync-localization.sh
@@ -7,7 +7,9 @@ echo "--- :rubygems: Setting up Gems"
 install_gems
 
 echo "--- :globe_with_meridians: :arrow_up: Generate the source language PO file for GlotPress based on `wp_localization/localization/en-US/main.ftl`"
-bundle exec fastlane generate_source_po_file commit_and_push_changes:true
+bundle exec fastlane generate_source_po_file commit_changes:true
 
 echo "--- :globe_with_meridians: :arrow_down: Download and update translations from GlotPress and update the local Fluent files"
-bundle exec fastlane download_translations commit_and_push_changes:true
+bundle exec fastlane download_translations commit_changes:true
+
+git push origin
