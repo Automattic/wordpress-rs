e52769e11cf21985aba05c38b001cafe9e514f11
Ian Maia <ian.maia@automattic.com>
2025-07-03 13:12:49 +0200

    Change localization sync to create a PR instead of committing to `trunk`

--- .buildkite/commands/sync-localization.sh
+++ .buildkite/commands/sync-localization.sh
@@ -11,10 +11,5 @@ source use-bot-for-git
 echo "--- :rubygems: Setting up Gems"
 install_gems
 
-echo "--- :globe_with_meridians: :arrow_up: Generate the source language PO file for GlotPress based on `wp_localization/localization/en-US/main.ftl`"
-bundle exec fastlane generate_source_po_file commit_changes:true
-
-echo "--- :globe_with_meridians: :arrow_down: Download and update translations from GlotPress and update the local Fluent files"
-bundle exec fastlane download_translations commit_changes:true
-
-git push origin
+echo "--- :globe_with_meridians: :arrows_counterclockwise: Synchronizing localization files with GlotPress"
+bundle exec fastlane sync_localization skip_confirm:true
