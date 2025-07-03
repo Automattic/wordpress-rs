342fd04e1f398fdf9167ae9f8a7618da10f10688
Ian G. Maia <iangmaia@users.noreply.github.com>
2025-07-03 11:18:55 +0200

    [Tooling] Add Localization sync job (#770)
    * Use latest release-toolkit Gem
    
    * Add Fluent / PO localization lanes
    
    * Update source PO file (en-US.pot) to be synced to GlotPress
    
    * Update generated Fluent files based on latest GlotPress translations
    
    * Update release-toolkit
    
    * Fix Rubocop violations
    
    * Run bundle update
    
    * Create temporary dir for downloading PO files and use open-uri for downloading
    
    * Update code to use `fluent-tools` Gem
    
    * Update generated source PO file
    
    * Update generated Fluent files based on latest GlotPress translations
    
    * Add job and script to sync localization files
    
    * Update lanes to not push automatically
    
    * Update sync script to only push at the end
    
    * Add localization README file
    
    * Reorder helper lane
    
    * Validate file_path at the beginning of the `generate_fluent_file_from_po` lane
    
    * Update code to exit early in `download_translations` when there are no downloaded files
    
    * Add all locales supported by the iOS and Android apps
    h/t to @crazytonyli for highlighting that the supported locales should be a combination of the locales supported in the iOS and Android apps https://github.com/Automattic/wordpress-rs/pull/765#discussion_r2151887208
    
    * Add generated Fluent files with the updated list of supported locales
    
    * Update fluent-tools to 0.2.0 and re-generate localization files with the latest version
    
    * Update to 0.3.0 release
    
    * Update PO file with latest fluent-tools
    
    * Add IMAGE to nightly localization sync job
    
    * Add ci-toolkit plugin to localization sync job
    
    * Make sure to checkout branch (given we're pushing later)
    
    * Update source PO file (en-US.pot) to be synced to GlotPress
    
    ---------
    
    Co-authored-by: Automattic Release Bot <mobile+wpmobilebot@automattic.com>

---
+++ .buildkite/commands/sync-localization.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -euo pipefail
+
+echo "--- :git: Checking out the current branch"
+BRANCH="${BUILDKITE_BRANCH:-trunk}"
+git checkout "${BRANCH}"
+git pull origin "${BRANCH}"
+
+echo '--- :robot_face: Use bot for Git operations'
+source use-bot-for-git
+
+echo "--- :rubygems: Setting up Gems"
+install_gems
+
+echo "--- :globe_with_meridians: :arrow_up: Generate the source language PO file for GlotPress based on `wp_localization/localization/en-US/main.ftl`"
+bundle exec fastlane generate_source_po_file commit_changes:true
+
+echo "--- :globe_with_meridians: :arrow_down: Download and update translations from GlotPress and update the local Fluent files"
+bundle exec fastlane download_translations commit_changes:true
+
+git push origin
