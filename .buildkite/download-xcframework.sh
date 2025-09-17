9aa05cfefd44ce01e25cbec480ee1d30f7ad4813
Tony Li <tony.li@automattic.com>
2025-09-17 15:27:36 +1200

    All swift files in wordpress-api-wrapper are auto-generated

--- .buildkite/download-xcframework.sh
+++ .buildkite/download-xcframework.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 
 echo "--- :arrow_down: Downloading xcframework"
 buildkite-agent artifact download target/libwordpressFFI.xcframework.zip . --step "xcframework"
-buildkite-agent artifact download native/swift/Sources/wordpress-api-wrapper/wp_api.swift . --step "xcframework"
-buildkite-agent artifact download native/swift/Sources/wordpress-api-wrapper/wp_localization.swift . --step "xcframework"
+buildkite-agent artifact download 'native/swift/Sources/wordpress-api-wrapper/*.swift' . --step "xcframework"
 unzip target/libwordpressFFI.xcframework.zip -d .
 rm target/libwordpressFFI.xcframework.zip
