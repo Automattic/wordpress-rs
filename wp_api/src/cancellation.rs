29fffc3f1911eca0e30112b3e15175eee50c3573
Tony Li <tony.li@automattic.com>
2025-09-17 12:42:34 +1200

    Remove the `wait_for_cancellation` API

--- wp_api/src/cancellation.rs
+++ wp_api/src/cancellation.rs
@@ -3,9 +3,6 @@ use std::{
     sync::{Arc, Mutex, PoisonError},
 };
 
-use futures::StreamExt;
-use futures::channel::mpsc::{TryRecvError, UnboundedSender, unbounded};
-
 use crate::uuid::WpUuid;
 
 #[uniffi::export(with_foreign)]
@@ -17,7 +14,6 @@ pub trait CancellationHandler: Send + Sync + std::fmt::Debug {
 pub struct CancellationToken {
     uuid: WpUuid,
     cancelled: Mutex<bool>,
-    waiters: Mutex<VecDeque<UnboundedSender<()>>>,
     handler: Mutex<VecDeque<Arc<dyn CancellationHandler>>>,
 }
 
@@ -28,7 +24,6 @@ impl CancellationToken {
         Self {
             uuid: WpUuid::default(),
             cancelled: Mutex::new(false),
-            waiters: Mutex::new(VecDeque::new()),
             handler: Mutex::new(VecDeque::new()),
         }
     }
@@ -46,22 +41,6 @@ impl CancellationToken {
         Ok(())
     }
 
-    pub async fn wait_for_cancellation(&self) -> Result<(), CancellationTokenError> {
-        if *self.cancelled.lock()? {
-            return Ok(());
-        }
-
-        let (sender, mut receiver) = unbounded();
-
-        {
-            let mut waiters = self.waiters.lock()?;
-            waiters.push_back(sender);
-        }
-
-        let _ = receiver.next().await;
-        Ok(())
-    }
-
     pub fn cancel(&self) -> Result<(), CancellationTokenError> {
         let mut cancelled = self.cancelled.lock()?;
         if *cancelled {
@@ -74,9 +53,6 @@ impl CancellationToken {
         while let Some(handler) = handlers.pop_front() {
             handler.cancelled();
         }
-
-        let mut waiters = self.waiters.lock()?;
-        waiters.retain_mut(|sender| sender.unbounded_send(()).is_ok());
 
         Ok(())
     }
@@ -94,95 +70,9 @@ impl<T> From<PoisonError<T>> for CancellationTokenError {
     }
 }
 
-impl From<TryRecvError> for CancellationTokenError {
-    fn from(_: TryRecvError) -> Self {
-        CancellationTokenError::Locking
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::time::Duration;
-    use tokio::time::{sleep, timeout};
-
-    #[tokio::test]
-    async fn test_wait_for_cancellation_returns_immediately_when_already_cancelled() {
-        let token = CancellationToken::new();
-        token.cancel().unwrap();
-
-        let result = token.wait_for_cancellation().await;
-        assert!(result.is_ok());
-    }
-
-    #[tokio::test]
-    async fn test_wait_for_cancellation_waits_until_cancelled() {
-        let token = CancellationToken::new();
-        let token_clone = std::sync::Arc::new(token);
-        let token_for_task = token_clone.clone();
-
-        let wait_task = tokio::spawn(async move { token_for_task.wait_for_cancellation().await });
-
-        sleep(Duration::from_millis(10)).await;
-        assert!(!wait_task.is_finished());
-
-        token_clone.cancel().unwrap();
-
-        let result = wait_task.await.unwrap();
-        assert!(result.is_ok());
-    }
-
-    #[tokio::test]
-    async fn test_wait_for_cancellation_multiple_waiters() {
-        let token = std::sync::Arc::new(CancellationToken::new());
-
-        let mut tasks = Vec::new();
-        for _ in 0..5 {
-            let token_clone = token.clone();
-            let task = tokio::spawn(async move { token_clone.wait_for_cancellation().await });
-            tasks.push(task);
-        }
-
-        sleep(Duration::from_millis(10)).await;
-        for task in &tasks {
-            assert!(!task.is_finished());
-        }
-
-        token.cancel().unwrap();
-
-        for task in tasks {
-            let result = task.await.unwrap();
-            assert!(result.is_ok());
-        }
-    }
-
-    #[tokio::test]
-    async fn test_wait_for_cancellation_timeout_when_not_cancelled() {
-        let token = CancellationToken::new();
-
-        let result = timeout(Duration::from_millis(50), token.wait_for_cancellation()).await;
-        assert!(result.is_err());
-    }
-
-    #[tokio::test]
-    async fn test_wait_for_cancellation_with_concurrent_cancel() {
-        let token = std::sync::Arc::new(CancellationToken::new());
-        let token_for_wait = token.clone();
-        let token_for_cancel = token.clone();
-
-        let wait_task = tokio::spawn(async move { token_for_wait.wait_for_cancellation().await });
-
-        let cancel_task = tokio::spawn(async move {
-            sleep(Duration::from_millis(20)).await;
-            token_for_cancel.cancel()
-        });
-
-        let wait_result = wait_task.await.unwrap();
-        let cancel_result = cancel_task.await.unwrap();
-
-        assert!(wait_result.is_ok());
-        assert!(cancel_result.is_ok());
-    }
 
     #[derive(Debug)]
     struct TestHandler {
@@ -253,26 +143,5 @@ mod tests {
         token.register_handler(Arc::new(handler)).unwrap();
 
         assert!(!called_flag.load(std::sync::atomic::Ordering::SeqCst));
-    }
-
-    #[tokio::test]
-    async fn test_register_handler_with_wait_for_cancellation() {
-        let token = std::sync::Arc::new(CancellationToken::new());
-        let (handler, called_flag) = TestHandler::new();
-
-        token.register_handler(Arc::new(handler)).unwrap();
-
-        let token_for_wait = token.clone();
-        let wait_task = tokio::spawn(async move { token_for_wait.wait_for_cancellation().await });
-
-        sleep(Duration::from_millis(10)).await;
-        assert!(!wait_task.is_finished());
-        assert!(!called_flag.load(std::sync::atomic::Ordering::SeqCst));
-
-        token.cancel().unwrap();
-
-        let result = wait_task.await.unwrap();
-        assert!(result.is_ok());
-        assert!(called_flag.load(std::sync::atomic::Ordering::SeqCst));
     }
 }
