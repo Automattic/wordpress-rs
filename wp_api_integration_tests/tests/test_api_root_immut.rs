2dfa084630a88d5f7e944c3fee3629c472fe66a4
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2025-09-22 15:52:30 -0600

    Add a WP.org test

---
+++ wp_api_integration_tests/tests/test_api_root_immut.rs
@@ -0,0 +1,16 @@
+
+use wp_api::wp_com::{endpoint::WpComDotOrgApiUrlResolver, WpComBaseUrl};
+use wp_api_integration_tests::prelude::*;
+
+#[tokio::test]
+#[parallel]
+async fn test_fetching_api_root() {
+    let response = api_client()
+    .api_root()
+    .get()
+    .await
+    .assert_response();
+
+    assert_eq!(response.data.url, "http://localhost");
+}
+}
