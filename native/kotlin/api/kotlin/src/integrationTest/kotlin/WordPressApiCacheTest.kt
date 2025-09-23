60409f379b7cc8ee235711f3706c399d65e764a1
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2025-09-23 15:09:16 -0600

    Add Kotlin background update notifications test

--- native/kotlin/api/kotlin/src/integrationTest/kotlin/WordPressApiCacheTest.kt
+++ native/kotlin/api/kotlin/src/integrationTest/kotlin/WordPressApiCacheTest.kt
@@ -1,16 +1,34 @@
 package rs.wordpress.api.cache.kotlin
 
+import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.parallel.Execution
 import org.junit.jupiter.api.parallel.ExecutionMode
 import rs.wordpress.cache.kotlin.WordPressApiCache
+import rs.wordpress.cache.kotlin.WordPressApiCacheDelegate
 import kotlin.test.assertEquals
 
 @Execution(ExecutionMode.CONCURRENT)
 class WordPressApiCacheTest {
 
     @Test
-    fun testThatMigrationsWork() {
+    fun testThatMigrationsWork() = runBlocking {
         assertEquals(2, WordPressApiCache().performMigrations())
+    }
+
+    @Test
+    fun testBackgroundUpdateNotificationsWork() = runBlocking {
+        var updateCount = 0
+        val delegate = WordPressApiCacheDelegate(
+            callback = { updateHook ->
+                updateCount += 1
+            }
+        )
+
+        val cache = WordPressApiCache(delegate = delegate)
+        cache.startListeningForUpdates()
+
+        val migrationCount = cache.performMigrations()
+        assertEquals(updateCount, migrationCount)
     }
 }
--- native/kotlin/api/kotlin/src/main/kotlin/rs/wordpress/cache/kotlin/WordPressApiCache.kt
+++ native/kotlin/api/kotlin/src/main/kotlin/rs/wordpress/cache/kotlin/WordPressApiCache.kt
@@ -1,44 +1,51 @@
 package rs.wordpress.cache.kotlin
 
 import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.withContext
 import uniffi.wp_api.DatabaseDelegate
 import uniffi.wp_api.UpdateHook
 import uniffi.wp_api.WpApiCache
-import uniffi.wp_api.setGlobalDelegate
 import java.nio.file.Path
 import java.util.concurrent.Executors
 
-class WordPressApiCacheDelegate : DatabaseDelegate {
+class WordPressApiCacheLoggingDelegate: DatabaseDelegate {
     override fun didUpdate(updateHook: UpdateHook) {
         println("Received update: $updateHook")
+    }
+}
+class WordPressApiCacheDelegate(
+    private val callback: (updateHook: UpdateHook) -> Unit
+) : DatabaseDelegate {
+
+    override fun didUpdate(updateHook: UpdateHook) {
+        callback(updateHook)
     }
 }
 
 class WordPressApiCache {
     private val cache: WpApiCache
     private val internalDispatcher = Executors.newSingleThreadExecutor().asCoroutineDispatcher()
-    private val delegate: DatabaseDelegate = WordPressApiCacheDelegate()
+    private val delegate: DatabaseDelegate?
 
     // Creates a new in-memory cache
-    constructor() : this(":memory:")
+    constructor(delegate: WordPressApiCacheDelegate? = null) : this(":memory:", delegate)
 
     // Creates a new cache at the specified file system URL
-    constructor(path: Path) : this(path.toString())
+    constructor(path: Path, delegate: WordPressApiCacheDelegate? = null) : this(path.toString(), delegate)
 
     // Creates a new cache at the specified path
-    constructor(string: String) {
+    constructor(string: String, delegate: WordPressApiCacheDelegate? = null) {
         this.cache = WpApiCache(string)
+        this.delegate = delegate
     }
 
-    fun performMigrations(): Int {
-        internalDispatcher.run {
-            return this@WordPressApiCache.cache.performMigrations().toInt()
-        }
+    suspend fun performMigrations(): Int = withContext(internalDispatcher) {
+        cache.performMigrations().toInt()
     }
-
     fun startListeningForUpdates() {
-        setGlobalDelegate(delegate)
-        this.cache.startListeningForUpdates()
+        if (this.delegate != null) {
+            this.cache.startListeningForUpdates(this.delegate)
+        }
     }
 
     fun stopListeningForUpdates() {
