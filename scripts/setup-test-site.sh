0b12a36e84e7fbea788e894794829ecb7a18f3af
Oguz Kocer <oguz.kocer@automattic.com>
2025-10-14 14:50:07 -0400

    Use latest Gutenberg plugin in test server

--- scripts/setup-test-site.sh
+++ scripts/setup-test-site.sh
@@ -71,9 +71,7 @@ wp import /tmp/testdata.xml --authors=create
 wp plugin deactivate wordpress-importer
 wp plugin delete wordpress-importer
 
-curl -sSL https://downloads.wordpress.org/plugin/gutenberg.21.7.0.zip -o /tmp/gutenberg.zip
-unzip -q /tmp/gutenberg.zip -d wp-content/plugins/
-wp plugin activate gutenberg
+wp plugin install gutenberg --activate
 
 # Install custom must-use plugins for integration tests
 mkdir -p wp-content/mu-plugins
