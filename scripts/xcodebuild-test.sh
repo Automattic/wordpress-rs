2f9ab0910d00c54968f85cb18b3a12d5006c3d0f
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2025-10-15 12:10:54 -0600

    Use Xcode 26.0 in CI

--- scripts/xcodebuild-test.sh
+++ scripts/xcodebuild-test.sh
@@ -7,6 +7,9 @@ set -euo pipefail
 platform=$1
 
 device_id=$(xcrun simctl list --json devices available | jq -re ".devices.\"com.apple.CoreSimulator.SimRuntime.${platform}\" | last.udid")
+device_name=$(xcrun simctl list --json devices available | jq -re ".devices.\"com.apple.CoreSimulator.SimRuntime.iOS-18-5\" | last.name")
+
+echo "Runing on device: ${device_name}"
 
 export NSUnbufferedIO=YES
 
