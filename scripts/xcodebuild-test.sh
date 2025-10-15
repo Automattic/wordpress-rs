8c27d35ad5dd168dcda72b41170d8a23f8370c9d
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2025-10-15 14:11:51 -0600

    Create simulators if they don’t already exist

--- scripts/xcodebuild-test.sh
+++ scripts/xcodebuild-test.sh
@@ -6,8 +6,23 @@ set -euo pipefail
 # runtime identifier returned by `xcrun simctl list runtimes`.
 platform=$1
 
-device_id=$(xcrun simctl list --json devices available | jq -re ".devices.\"com.apple.CoreSimulator.SimRuntime.${platform}\" | last.udid")
-device_name=$(xcrun simctl list --json devices available | jq -re ".devices.\"com.apple.CoreSimulator.SimRuntime.iOS-18-5\" | last.name")
+runtime="com.apple.CoreSimulator.SimRuntime.${platform}"
+
+simcount=$(xcrun simctl list --json devices available | jq -re ".devices.\"${runtime}\" | length")
+
+if [ $simcount -eq 0 ]; then
+    echo "No simulators found for ${platform}. Creating simulator"
+    if [ $platform == "iOS"* ]; then
+        xcrun simctl create "iPhone 17 Pro Test Device" "com.apple.CoreSimulator.SimDeviceType.iPhone-17-Pro"
+    elif [ $platform == "tvOS"* ]; then
+        xcrun simctl create "Apple TV 4K at 1080p Test Device" "com.apple.CoreSimulator.SimDeviceType.Apple-TV-4K-1080p"
+    elif [ $platform == "watchOS"* ]; then
+        xcrun simctl create "Apple Watch Series 11 Test Device" "com.apple.CoreSimulator.SimDeviceType.Apple-Watch-Series-11-46mm"
+    fi
+fi
+
+device_id=$(xcrun simctl list --json devices available | jq -re ".devices.\"${runtime}\" | last.udid")
+device_name=$(xcrun simctl list --json devices available | jq -re ".devices.\"${runtime}\" | last.name")
 
 echo "Runing on device: ${device_name}"
 
