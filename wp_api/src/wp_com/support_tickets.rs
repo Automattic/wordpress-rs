24bca97119f6a213bd7a25378786a1c9cdd24a01
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2025-10-28 14:18:28 -0600

    Make support attachment dimensions strongly typed (#983)

--- wp_api/src/wp_com/support_tickets.rs
+++ wp_api/src/wp_com/support_tickets.rs
@@ -90,7 +90,7 @@ pub struct SupportAttachment {
     pub content_type: String,
     pub size: u64,
     pub url: String,
-    pub metadata: HashMap<String, AttachmentMetadataValue>,
+    pub metadata: HashMap<AttachmentMetadataKey, AttachmentMetadataValue>,
 }
 
 #[derive(Debug, PartialEq, Eq, Serialize, Deserialize, uniffi::Enum)]
@@ -100,12 +100,25 @@ pub enum SupportMessageAuthor {
     SupportAgent(SupportAgentIdentity),
 }
 
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize, uniffi::Enum, strum_macros::Display)]
-#[strum(serialize_all = "snake_case")]
+#[derive(
+    Debug,
+    Hash,
+    PartialEq,
+    Eq,
+    Serialize,
+    Deserialize,
+    uniffi::Enum,
+    strum_macros::Display,
+    strum_macros::EnumString,
+)]
 pub enum AttachmentMetadataKey {
+    #[serde(alias = "width")]
     Width,
+    #[serde(alias = "height")]
     Height,
-    Other(String),
+    #[serde(untagged)]
+    #[strum(default)]
+    Custom(String),
 }
 
 #[derive(Debug, PartialEq, Eq, Serialize, Deserialize, uniffi::Enum)]
@@ -114,6 +127,40 @@ pub enum AttachmentMetadataValue {
     String(String),
     Number(u64),
     Boolean(bool),
+}
+
+impl AttachmentMetadataValue {
+    pub fn get_number(&self) -> Option<u64> {
+        match self {
+            AttachmentMetadataValue::Number(number) => Some(*number),
+            _ => None,
+        }
+    }
+}
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize, uniffi::Record)]
+pub struct AttachmentDimensions {
+    pub width: u64,
+    pub height: u64,
+}
+
+#[uniffi::export]
+pub fn get_attachment_dimensions(attachment: &SupportAttachment) -> Option<AttachmentDimensions> {
+    let metadata = &attachment.metadata;
+
+    let width = metadata
+        .get(&AttachmentMetadataKey::Width)
+        .and_then(|v| v.get_number());
+    let height = metadata
+        .get(&AttachmentMetadataKey::Height)
+        .and_then(|v| v.get_number());
+
+    if let Some(width) = width
+        && let Some(height) = height
+    {
+        return Some(AttachmentDimensions { width, height });
+    }
+
+    None
 }
 
 #[derive(Debug, PartialEq, Eq, Serialize, Deserialize, uniffi::Record)]
@@ -151,6 +198,7 @@ impl std::fmt::Display for ConversationId {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rstest::*;
 
     #[test]
     fn test_support_conversation_deserialization() {
@@ -166,5 +214,84 @@ mod tests {
         let conversation_list: Vec<SupportConversationSummary> =
             serde_json::from_str(json).expect("Failed to deserialize support conversation list");
         assert_eq!(conversation_list.len(), 11);
+    }
+
+    #[test]
+    fn test_support_conversation_with_attachments_deserialization() {
+        let json = include_str!(
+            "../../tests/wpcom/support_tickets/single-conversation-with-attachments.json"
+        );
+        let conversation: SupportConversation =
+            serde_json::from_str(json).expect("Failed to deserialize support conversation");
+        assert_eq!(conversation.messages.len(), 1);
+        assert_eq!(conversation.messages[0].attachments.len(), 2);
+        assert_eq!(
+            conversation.messages[0].attachments[0].filename,
+            "sample-image-1.jpg"
+        );
+        assert_eq!(
+            conversation.messages[0].attachments[0].content_type,
+            "image/jpeg"
+        );
+        assert_eq!(conversation.messages[0].attachments[0].size, 123456);
+        assert_eq!(
+            conversation.messages[0].attachments[0].url,
+            "https://example.com/attachments/token/token1/?name=sample-image-1.jpg"
+        );
+
+        let dimensions =
+            get_attachment_dimensions(&conversation.messages[0].attachments[0]).unwrap();
+
+        assert_eq!(dimensions.width, 1000);
+        assert_eq!(dimensions.height, 800);
+
+        assert_eq!(
+            conversation.messages[0].attachments[1].filename,
+            "sample-image-2.jpg"
+        );
+        assert_eq!(
+            conversation.messages[0].attachments[1].content_type,
+            "image/jpeg"
+        );
+        assert_eq!(conversation.messages[0].attachments[1].size, 654321);
+        assert_eq!(
+            conversation.messages[0].attachments[1].url,
+            "https://example.com/attachments/token/token2/?name=sample-image-2.jpg"
+        );
+
+        let dimensions =
+            get_attachment_dimensions(&conversation.messages[0].attachments[1]).unwrap();
+        assert_eq!(dimensions.width, 2000);
+        assert_eq!(dimensions.height, 1600);
+    }
+
+    #[test]
+    fn test_attachment_metadata_key_custom_deserialization() {
+        let json = r#"{"Custom": "test"}"#;
+        let metadata: HashMap<AttachmentMetadataKey, AttachmentMetadataValue> =
+            serde_json::from_str(json).expect("Failed to deserialize attachment metadata");
+        assert_eq!(metadata.len(), 1);
+    }
+
+    #[rstest]
+    #[case(AttachmentMetadataKey::Width, "Width")]
+    #[case(AttachmentMetadataKey::Height, "Height")]
+    #[case(
+        AttachmentMetadataKey::Custom(String::from("testlowercase")),
+        "testlowercase"
+    )]
+    #[case(
+        AttachmentMetadataKey::Custom(String::from("testWithCamelCase")),
+        "testWithCamelCase"
+    )]
+    #[case(
+        AttachmentMetadataKey::Custom(String::from("test_with_snake_case")),
+        "test_with_snake_case"
+    )]
+    fn test_attachment_metadata_key_to_string(
+        #[case] key: AttachmentMetadataKey,
+        #[case] expected_str: &str,
+    ) {
+        assert_eq!(key.to_string(), expected_str);
     }
 }
