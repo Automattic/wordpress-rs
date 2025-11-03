1ebeeacd2a78cded78e2a81d1029d12ee2b596db
Tony Li <tony.li@automattic.com>
2025-11-03 15:21:47 +1300

    Update the installing swift script

--- scripts/docker/install-swift.sh
+++ scripts/docker/install-swift.sh
@@ -2,11 +2,12 @@
 
 set -euo pipefail
 
-curl -s -o swiftly.tar.gz https://download.swift.org/swiftly/linux/swiftly-aarch64.tar.gz
+curl -s -o swiftly.tar.gz "https://download.swift.org/swiftly/linux/swiftly-$(uname -m).tar.gz"
 tar zxf swiftly.tar.gz
 rm swiftly.tar.gz
-./swiftly init --platform=debian12 --assume-yes --skip-install
+./swiftly init --assume-yes --skip-install
 
 apt-get -y -qq install libicu-dev libcurl4-openssl-dev libedit-dev libsqlite3-dev libncurses-dev libpython3-dev libxml2-dev uuid-dev git libstdc++-12-dev
 
-swiftly install --use 6.1
+echo "Installing Swift..."
+swiftly install --progress-file /dev/null --use 6.1
