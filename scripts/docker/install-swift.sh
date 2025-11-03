8155b69b04c81e442982a04ebdf3deac2ef7a013
Tony Li <tony.li@automattic.com>
2025-11-03 15:31:52 +1300

    Move Swift Linux test job to the end-to-end test group

--- scripts/docker/install-swift.sh
+++ scripts/docker/install-swift.sh
@@ -5,6 +5,9 @@ set -euo pipefail
 curl -s -o swiftly.tar.gz "https://download.swift.org/swiftly/linux/swiftly-$(uname -m).tar.gz"
 tar zxf swiftly.tar.gz
 rm swiftly.tar.gz
+
+# If you get an "Unsupported Linux platform" error, check out the supported platforms here:
+# https://github.com/swiftlang/swiftly/blob/1.1.0/Sources/LinuxPlatform/Linux.swift#L12-L20
 ./swiftly init --assume-yes --skip-install
 
 apt-get -y -qq install libicu-dev libcurl4-openssl-dev libedit-dev libsqlite3-dev libncurses-dev libpython3-dev libxml2-dev uuid-dev git libstdc++-12-dev
