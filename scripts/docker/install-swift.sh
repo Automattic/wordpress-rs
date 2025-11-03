c666747e1d37cf9fed70b965bb19aeb59de960db
Tony Li <tony.li@automattic.com>
2025-11-03 15:06:04 +1300

    Less console output during building docker image

--- scripts/docker/install-swift.sh
+++ scripts/docker/install-swift.sh
@@ -2,11 +2,11 @@
 
 set -euo pipefail
 
-curl -o swiftly.tar.gz https://download.swift.org/swiftly/linux/swiftly-aarch64.tar.gz
+curl -s -o swiftly.tar.gz https://download.swift.org/swiftly/linux/swiftly-aarch64.tar.gz
 tar zxf swiftly.tar.gz
 rm swiftly.tar.gz
 ./swiftly init --platform=debian12 --assume-yes --skip-install
 
-apt-get -y install libicu-dev libcurl4-openssl-dev libedit-dev libsqlite3-dev libncurses-dev libpython3-dev libxml2-dev uuid-dev git libstdc++-12-dev
+apt-get -y -qq install libicu-dev libcurl4-openssl-dev libedit-dev libsqlite3-dev libncurses-dev libpython3-dev libxml2-dev uuid-dev git libstdc++-12-dev
 
 swiftly install --use 6.1
