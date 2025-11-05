a007357131800caa1a04f971b69283f42cc5f571
Gio Lodi <gio@mokacoding.com>
2025-11-05 20:51:10 +1100

    Update `download-xcframework.sh` after changing ZIP location

--- .buildkite/download-xcframework.sh
+++ .buildkite/download-xcframework.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 echo "--- :arrow_down: Downloading xcframework"
-buildkite-agent artifact download target/libwordpressFFI.xcframework.zip . --step "xcframework"
+buildkite-agent artifact download libwordpressFFI.xcframework.zip . --step "xcframework"
 buildkite-agent artifact download 'native/swift/Sources/wordpress-api-wrapper/*.swift' . --step "xcframework"
-unzip target/libwordpressFFI.xcframework.zip -d .
-rm target/libwordpressFFI.xcframework.zip
+unzip libwordpressFFI.xcframework.zip -d .
+rm libwordpressFFI.xcframework.zip
