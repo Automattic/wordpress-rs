2acadaa74bed532699d56a6f6523570fa54dda56
Gio Lodi <gio@mokacoding.com>
2025-11-05 21:21:57 +1100

    Temporarily also download XCFramework ZIP to target

--- .buildkite/download-xcframework.sh
+++ .buildkite/download-xcframework.sh
@@ -4,6 +4,14 @@ set -euo pipefail
 
 echo "--- :arrow_down: Downloading xcframework"
 buildkite-agent artifact download libwordpressFFI.xcframework.zip . --step "xcframework"
+unzip libwordpressFFI.xcframework.zip -d .
+rm libwordpressFFI.xcframework.zip
+
 buildkite-agent artifact download 'native/swift/Sources/wordpress-api-wrapper/*.swift' . --step "xcframework"
+
+# Temporarily also download to target folder because some parts still expect it there
+mkdir -p ./target
+pushd target
+buildkite-agent artifact download libwordpressFFI.xcframework.zip . --step "xcframework"
 unzip libwordpressFFI.xcframework.zip -d .
 rm libwordpressFFI.xcframework.zip
