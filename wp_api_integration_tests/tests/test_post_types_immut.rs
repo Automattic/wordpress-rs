ea3f59b595503b339cce20060b1a7c29dbd89c3c
Oguz Kocer <oguz.kocer@automattic.com>
2025-11-28 11:01:39 -0500

    Use JsonValue for post type supports field
    WordPress 6.9 returns post type supports as either bool or array,
    so we use JsonValue wrapped in PostTypeSupportsValue for flexibility.
    
    Changes:
    - Add PostTypeSupportsValue type wrapping JsonValue
    - Update PostTypeSupportsMap to use Arc<PostTypeSupportsValue>
    - Update test assertion to use as_json_bool() method

--- wp_api_integration_tests/tests/test_post_types_immut.rs
+++ wp_api_integration_tests/tests/test_post_types_immut.rs
@@ -93,8 +93,12 @@ async fn retrieve_post_types_with_edit_context(
     // post types might not support `Title` in which case it's perfectly fine to completely
     // remove this assertion.
     assert_eq!(
-        post_type.supports.map.get(&PostTypeSupports::Title),
-        Some(true).as_ref()
+        post_type
+            .supports
+            .map
+            .get(&PostTypeSupports::Title)
+            .and_then(|v| v.as_json_bool()),
+        Some(true)
     );
     // All post types in our current testing sites have `EditPost` capability, so we use this
     // assertion to verify that we are able to parse `capabilities` field properly.
