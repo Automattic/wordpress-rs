9fd100c70eaa43f0cb93a0c7f9da27d906bb62d7
Oguz Kocer <oguz.kocer@automattic.com>
2026-05-07 20:35:00 -0400

    Fix filter tests for `/block-patterns/patterns`
    Use proper `filter` module with generated test case macros and
    `assert_that_instance_fields_nullability_match_provided_fields`
    for all three contexts.

--- wp_api_integration_tests/tests/test_block_patterns_immut.rs
+++ wp_api_integration_tests/tests/test_block_patterns_immut.rs
@@ -1,4 +1,7 @@
-use wp_api::block_patterns::SparseBlockPatternFieldWithViewContext;
+use wp_api::block_patterns::{
+    SparseBlockPatternFieldWithEditContext, SparseBlockPatternFieldWithEmbedContext,
+    SparseBlockPatternFieldWithViewContext,
+};
 use wp_api_integration_tests::prelude::*;
 
 #[tokio::test]
@@ -33,25 +36,67 @@ async fn list_with_view_context() {
         .assert_response();
 }
 
-#[tokio::test]
-#[parallel]
-async fn filter_list_with_view_context() {
-    let patterns = api_client()
-        .block_patterns()
-        .filter_list_with_view_context(&[
-            SparseBlockPatternFieldWithViewContext::Name,
-            SparseBlockPatternFieldWithViewContext::Title,
-        ])
-        .await
-        .assert_response()
-        .data;
-    assert!(!patterns.is_empty());
-    // Fields not requested should be None
-    assert!(
-        patterns
-            .first()
-            .expect("should have results")
-            .content
-            .is_none()
-    );
+mod filter {
+    use super::*;
+
+    wp_api::generate_sparse_block_pattern_field_with_edit_context_test_cases!();
+    wp_api::generate_sparse_block_pattern_field_with_embed_context_test_cases!();
+    wp_api::generate_sparse_block_pattern_field_with_view_context_test_cases!();
+
+    #[apply(sparse_block_pattern_field_with_edit_context_test_cases)]
+    #[case(&[SparseBlockPatternFieldWithEditContext::Name, SparseBlockPatternFieldWithEditContext::Title])]
+    #[tokio::test]
+    #[parallel]
+    async fn filter_list_with_edit_context(
+        #[case] fields: &[SparseBlockPatternFieldWithEditContext],
+    ) {
+        api_client()
+            .block_patterns()
+            .filter_list_with_edit_context(fields)
+            .await
+            .assert_response()
+            .data
+            .iter()
+            .for_each(|pattern| {
+                pattern.assert_that_instance_fields_nullability_match_provided_fields(fields)
+            });
+    }
+
+    #[apply(sparse_block_pattern_field_with_embed_context_test_cases)]
+    #[case(&[SparseBlockPatternFieldWithEmbedContext::Name, SparseBlockPatternFieldWithEmbedContext::Title])]
+    #[tokio::test]
+    #[parallel]
+    async fn filter_list_with_embed_context(
+        #[case] fields: &[SparseBlockPatternFieldWithEmbedContext],
+    ) {
+        api_client()
+            .block_patterns()
+            .filter_list_with_embed_context(fields)
+            .await
+            .assert_response()
+            .data
+            .iter()
+            .for_each(|pattern| {
+                pattern.assert_that_instance_fields_nullability_match_provided_fields(fields)
+            });
+    }
+
+    #[apply(sparse_block_pattern_field_with_view_context_test_cases)]
+    #[case(&[SparseBlockPatternFieldWithViewContext::Name, SparseBlockPatternFieldWithViewContext::Title])]
+    #[tokio::test]
+    #[parallel]
+    async fn filter_list_with_view_context(
+        #[case] fields: &[SparseBlockPatternFieldWithViewContext],
+    ) {
+        api_client()
+            .block_patterns()
+            .filter_list_with_view_context(fields)
+            .await
+            .assert_response()
+            .data
+            .iter()
+            .for_each(|pattern| {
+                pattern.assert_that_instance_fields_nullability_match_provided_fields(fields)
+            });
+    }
 }
